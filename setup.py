37acebd2c68f93e54e200ce589f3fbc89913abb9
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2018-05-28 13:01:04 -0700

    Add scipy dependency

--- setup.py
+++ setup.py
@@ -58,5 +58,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.11.3', 'scikit-learn>=0.18'],
+    install_requires=['numpy>=1.11.3', 'scikit-learn>=0.18', 'scipy>=1.1.0'],
 )
