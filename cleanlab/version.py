26ce101b0d1627d67debf8302eb553064e0a74f3
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2018-11-22 16:45:07 -0500

    0.0.3 - adding link to logo that works with pypi

--- cleanlab/version.py
+++ cleanlab/version.py
@@ -1,1 +1,5 @@
-__version__ = '0.0.1'
+__version__ = '0.0.3'
+
+# 0.0.3 - Adding working logo to README, pypi working
+# 0.0.2 - Added logo to README, but link does not load on pypi
+# 0.0.1 - initial commit
