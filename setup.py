4deb9b0db4e0657ba5ce232c4ba1ab18146c6bca
Curtis Northcutt <curtis.northcutt@gmail.com>
2018-11-24 12:00:28 -0500

    bug fix: made explicit declaration of version a string

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 # Get version number
 # exec(open('cleanlab/version.py').read())
-__version__ = 0.0.3
+__version__ = '0.0.3'
 
 setup(
     name='cleanlab',
