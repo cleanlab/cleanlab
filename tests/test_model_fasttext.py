28388082a9c8b1f73d168a5e9b38fed13f6ddf62
Curtis Northcutt <curtis.northcutt@gmail.com>
2018-12-03 00:22:46 -0800

    Added version warning for fasttext and unsupported  python 2

--- tests/test_model_fasttext.py
+++ tests/test_model_fasttext.py
@@ -1,20 +1,31 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 # Python 2 and 3 compatibility
 from __future__ import print_function, absolute_import, division, unicode_literals, with_statement
 
 
-# In[2]:
+# In[ ]:
+
+
+# Make sure python version is compatible with fasttext
+from cleanlab.util import VersionWarning
+python_version = VersionWarning(
+    warning_str = "fastText supports Python 3 versions (not python 2).",
+    list_of_compatible_versions = [3.4, 3.5, 3.6],
+)
+
+
+# In[ ]:
 
 
 DATA_DIR = 'fasttext_data/'
 
 
-# In[3]:
+# In[ ]:
 
 
 def create_cooking_dataset(data_dir = None):
@@ -76,13 +87,10 @@ def create_cooking_dataset(data_dir = None):
         shutil.rmtree('cooking')
 
 
-# In[4]:
+# In[ ]:
 
 
-# fasttext only exists for these versions that are also compatible with cleanlab
-import sys
-v = sys.version_info[0] + 0.1 * sys.version_info[1]
-if v in [3.4, 3.5, 3.6]:
+if python_version.is_compatible():
     from fastText import train_supervised
     from cleanlab.models.fasttext import FastTextClassifier
     from sklearn.metrics import accuracy_score
@@ -111,44 +119,43 @@ if v in [3.4, 3.5, 3.6]:
         },
         del_intermediate_data = True,
     )
-    ftc.fit(X = None)
-else:
-    import warnings
-    warning = '''\n    fastText only supports Python 3.
-    cleanlab supports Python versions 2.7, 3.4, 3.5, and 3.6.
-    You are using Python version {}. To use cleanlab with fasttext, 
-    you'll need to use Python 3.4, 3.5, or 3.6.'''.format(v)
-    warnings.warn(warning)    
+    ftc.fit(X = None)   
 
 
-# In[5]:
+# In[ ]:
 
 
 def test_predict_proba_masking():
-    ''''''
-    psx = ftc.predict_proba(X = [500, 1000, 4999])
-    assert(psx.shape[0] == 3)
+    
+    if python_version.is_compatible():
+        psx = ftc.predict_proba(X = [500, 1000, 4999])
+        assert(psx.shape[0] == 3)
+    assert(True)
 
 
-# In[6]:
+# In[ ]:
 
 
 def test_predict_masking():
-    ''''''
-    pred = ftc.predict(X = [500, 1000, 4999])
-    assert(pred.shape[0] == 3)
+    
+    if python_version.is_compatible():
+        pred = ftc.predict(X = [500, 1000, 4999])
+        assert(pred.shape[0] == 3)
+    assert(True)
 
 
-# In[7]:
+# In[ ]:
 
 
 def test_score_masking():
-    ''''''
-    score = ftc.score(X = [4, 8,  500, 1000, 4999], k = 5)
-    assert(0. <= score <= 1.0)
+    
+    if python_version.is_compatible():
+        score = ftc.score(X = [4, 8,  500, 1000, 4999], k = 5)
+        assert(0. <= score <= 1.0)
+    assert(True)
 
 
-# In[8]:
+# In[ ]:
 
 
 def test_apk_strictly_increasing():
@@ -156,15 +163,18 @@ def test_apk_strictly_increasing():
     we check if the true label is on the top k of the predicted
     labels. Thus, as k is increased, our accuracy should only increase
     or stay the same.'''
-    prev_score = 0
-    for k in range(1, 10):
-        score = ftc.score(X = range(500), k = k)
-        assert(score >= prev_score)
-        prev_score = score
-        print(prev_score)
+    
+    if python_version.is_compatible():
+        prev_score = 0
+        for k in range(1, 10):
+            score = ftc.score(X = range(500), k = k)
+            assert(score >= prev_score)
+            prev_score = score
+            print(prev_score)
+    assert(True)
 
 
-# In[9]:
+# In[ ]:
 
 
 def test_predict_and_predict_proba():
@@ -174,20 +184,20 @@ def test_predict_and_predict_proba():
     of if we use the fasttext model or our class to 
     predict the labels and the probabilities.'''
     
-    # Check labels
-    us = ftc.predict(train_data = False)
-    them = [ftc.label2num[z[0]] for z in ftc.clf.predict(text)[0]]
-    assert(accuracy_score(us, them) == 1)
-    
-    # Check probabilities
-    us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
-    them_prob = ftc.clf.predict(text, k = len(us_prob))[1].max(axis = 1)
-    assert(np.sum((us_prob - them_prob)**2) < 1e-4)
-    
-    return ftc
+    if python_version.is_compatible():
+        # Check labels
+        us = ftc.predict(train_data = False)
+        them = [ftc.label2num[z[0]] for z in ftc.clf.predict(text)[0]]
+        assert(accuracy_score(us, them) == 1)
+
+        # Check probabilities
+        us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
+        them_prob = ftc.clf.predict(text, k = len(us_prob))[1].max(axis = 1)
+        assert(np.sum((us_prob - them_prob)**2) < 1e-4)
+    assert(True)
 
 
-# In[11]:
+# In[ ]:
 
 
 def test_predict_and_predict_proba():
@@ -197,125 +207,106 @@ def test_predict_and_predict_proba():
     of if we use the fasttext model or our class to 
     predict the labels and the probabilities.'''
     
-    # Check labels
-    us = ftc.predict(train_data = False)
-    them = [ftc.label2num[z[0]] for z in ftc.clf.predict(text)[0]]
-    assert(accuracy_score(us, them) == 1)
-    
-    # Check probabilities
-    us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
-    them_prob = ftc.clf.predict(text, k = len(us_prob))[1].max(axis = 1)
-    assert(np.sum((us_prob - them_prob)**2) < 1e-4)
-    
-    return ftc
+    if python_version.is_compatible():
+        # Check labels
+        us = ftc.predict(train_data = False)
+        them = [ftc.label2num[z[0]] for z in ftc.clf.predict(text)[0]]
+        assert(accuracy_score(us, them) == 1)
+
+        # Check probabilities
+        us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
+        them_prob = ftc.clf.predict(text, k = len(us_prob))[1].max(axis = 1)
+        assert(np.sum((us_prob - them_prob)**2) < 1e-4)
+    assert(True)
 
 
-# In[12]:
+# In[ ]:
 
 
 def test_correctness():
     '''Test to see if our model produces exactly the 
     same results as the '''
     
-    with open(DATA_DIR + 'cooking.test.txt', 'r') as f:
-        test_data = [z.strip() for z in f.readlines()]
-    _, text = [list(t) for t in zip(*(z.split(" ", 1) for z in test_data))]
-    
-    ftc = FastTextClassifier(
-        train_data_fn = DATA_DIR + 'cooking.train.txt', 
-        test_data_fn = DATA_DIR + 'cooking.test.txt',
-        kwargs_train_supervised = {
-            'epoch': 5,
-        },
-        del_intermediate_data = True,
-    )
-    ftc.fit(X = None)
-    
-    original = train_supervised(DATA_DIR + 'cooking.train.txt', )
-    
-    # Check labels
-    us = ftc.predict(train_data = False)
-    them = [ftc.label2num[z[0]] for z in original.predict(text)[0]]
-    assert(accuracy_score(us, them) == 1)
-    
-    # Check probabilities
-    us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
-    them_prob = original.predict(text, k = len(us_prob))[1].max(axis = 1)
-    assert(np.sum((us_prob - them_prob)**2) < 1e-4)
+    if python_version.is_compatible():
+        with open(DATA_DIR + 'cooking.test.txt', 'r') as f:
+            test_data = [z.strip() for z in f.readlines()]
+        _, text = [list(t) for t in zip(*(z.split(" ", 1) for z in test_data))]
+
+        ftc = FastTextClassifier(
+            train_data_fn = DATA_DIR + 'cooking.train.txt', 
+            test_data_fn = DATA_DIR + 'cooking.test.txt',
+            kwargs_train_supervised = {
+                'epoch': 5,
+            },
+            del_intermediate_data = True,
+        )
+        ftc.fit(X = None)
+
+        original = train_supervised(DATA_DIR + 'cooking.train.txt', )
+
+        # Check labels
+        us = ftc.predict(train_data = False)
+        them = [ftc.label2num[z[0]] for z in original.predict(text)[0]]
+        assert(accuracy_score(us, them) == 1)
+
+        # Check probabilities
+        us_prob = ftc.predict_proba(train_data = False).max(axis = 1)
+        them_prob = original.predict(text, k = len(us_prob))[1].max(axis = 1)
+        assert(np.sum((us_prob - them_prob)**2) < 1e-4)
+    assert(True)
 
 
-# In[14]:
-
-
-
-
-
-# In[15]:
-
-
-import cleanlab
-
-# Pre-train
-ftc = FastTextClassifier(
-    train_data_fn= DATA_DIR + 'cooking.train.txt', 
-    test_data_fn='/Users/cgn/data/cooking/cooking.test.txt',
-    kwargs_train_supervised = {
-        'epoch': 10,
-    },
-    del_intermediate_data=True,
-)
-ftc.fit()
-ftc.clf.epoch = 1
-psx = ftc.predict_proba(train_data = False)
-
-
-# In[179]:
+# In[ ]:
 
 
 def test_cleanlab_with_fasttext():
-    import cleanlab
+    '''Tests FastTextClassifier when used with cleanlab to find a label error.'''
+    
+    if python_version.is_compatible():
+        import cleanlab
 
-    top = 3
-    label_counts = list(zip(np.unique(y_train + y_test), cleanlab.util.value_counts(y_train + y_test)))
-    # Find which labels occur the most often.
-    top_labels = [v for v,c in sorted(label_counts, key=lambda x: x[1])[::-1][:top]]
+        top = 3
+        label_counts = list(zip(np.unique(y_train + y_test), cleanlab.util.value_counts(y_train + y_test)))
+        # Find which labels occur the most often.
+        top_labels = [v for v,c in sorted(label_counts, key=lambda x: x[1])[::-1][:top]]
 
-    # Get indices of data and labels for the top labels
-    X_train_idx, y_train_top = [list(w) for w in zip(*[(i, z.split(" ", 1)[0]) for i, z in enumerate(train_data) if z.split(" ", 1)[0] in top_labels])]
-    X_test_idx, y_test_top = [list(w) for w in zip(*[(i, z.split(" ", 1)[0]) for i, z in enumerate(test_data) if z.split(" ", 1)[0] in top_labels])]
+        # Get indices of data and labels for the top labels
+        X_train_idx, y_train_top = [list(w) for w in zip(*[(i, z.split(" ", 1)[0]) for i, z in enumerate(train_data) if z.split(" ", 1)[0] in top_labels])]
+        X_test_idx, y_test_top = [list(w) for w in zip(*[(i, z.split(" ", 1)[0]) for i, z in enumerate(test_data) if z.split(" ", 1)[0] in top_labels])]
 
-    # Pre-train
-    ftc = FastTextClassifier(
-        train_data_fn = DATA_DIR + 'cooking.train.txt', 
-        test_data_fn = DATA_DIR + 'cooking.test.txt', 
-        kwargs_train_supervised = {
-            'epoch': 20,
-        },
-        del_intermediate_data = True,
-    )
-    ftc.fit(X_train_idx, y_train_top)
-    # Set epochs to 1 for getting cross-validated predicted probabilities
-    ftc.clf.epoch = 1
+        # Pre-train
+        ftc = FastTextClassifier(
+            train_data_fn = DATA_DIR + 'cooking.train.txt', 
+            test_data_fn = DATA_DIR + 'cooking.test.txt', 
+            kwargs_train_supervised = {
+                'epoch': 20,
+            },
+            del_intermediate_data = True,
+        )
+        ftc.fit(X_train_idx, y_train_top)
+        # Set epochs to 1 for getting cross-validated predicted probabilities
+        ftc.clf.epoch = 1
 
-    # Dictionary mapping string labels to non-negative integers 0, 1, 2...
-    label2num = dict(zip(np.unique(y_train_top), range(top)))
-    # Map labels
-    s_train = np.array([label2num[z] for z in y_train_top])
-    # Compute confident joint and predicted probability matrix for each example
-    cj, psx = estimate_confident_joint_and_cv_pred_proba(X = np.array(X_train_idx), s = s_train, clf = ftc, cv_n_folds=5)
-    # Find inidices of errors
-    noise_idx = cleanlab.pruning.get_noise_indices(
-        s_train, 
-        psx, 
-        confident_joint=cj, 
-        prune_count_method='calibrate_confident_joint',
-        prune_method = 'prune_by_class'
-    )
-    # Extract errors. This works by:
-    # (1) masking the training examples we used with the noise indices identified.
-    # (2) we find the actual train_data corresponding to those indices.
-    errors = np.array(train_data)[np.array(X_train_idx)[noise_idx]]
+        # Dictionary mapping string labels to non-negative integers 0, 1, 2...
+        label2num = dict(zip(np.unique(y_train_top), range(top)))
+        # Map labels
+        s_train = np.array([label2num[z] for z in y_train_top])
+        # Compute confident joint and predicted probability matrix for each example
+        cj, psx = estimate_confident_joint_and_cv_pred_proba(X = np.array(X_train_idx), s = s_train, clf = ftc, cv_n_folds=5)
+        # Find inidices of errors
+        noise_idx = cleanlab.pruning.get_noise_indices(
+            s_train, 
+            psx, 
+            confident_joint=cj, 
+            prune_count_method='calibrate_confident_joint',
+            prune_method = 'prune_by_class'
+        )
+        # Extract errors. This works by:
+        # (1) masking the training examples we used with the noise indices identified.
+        # (2) we find the actual train_data corresponding to those indices.
+        errors = np.array(train_data)[np.array(X_train_idx)[noise_idx]]
 
-    # Known error - this should be tagged as substituion, not baking.
-    assert('__label__baking what can i use instead of corn syrup ?' in errors)
+        # Known error - this should be tagged as substituion, not baking.
+        assert('__label__baking what can i use instead of corn syrup ?' in errors)
+    assert(True)
 
