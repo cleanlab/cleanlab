185b7115579af99a3fff8d01c89790ab17608b99
Curtis Northcutt <curtis.northcutt@gmail.com>
2018-12-02 22:52:00 -0800

    Add script to download stackexchange data for fasttext nlp example

---
+++ tests/get_cooking_stackexchange_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DATADIR=$1
+
+echo "Downloading cooking dataset to ${DATADIR}"
+
+data_result="${DATADIR}"/cooking/cooking.stackexchange.txt
+if [ ! -f "$data_result" ]
+then
+  mkdir -p "${DATADIR}"/cooking/
+  wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/cooking.stackexchange.tar.gz -O "${DATADIR}"/cooking/cooking.stackexchange.tar.gz
+  tar xvzf "${DATADIR}"/cooking/cooking.stackexchange.tar.gz --directory "${DATADIR}"/cooking || exit 1
+  cat "${DATADIR}"/cooking/cooking.stackexchange.txt | sed -e "s/\([.\!?,'/()]\)/ \1 /g" | tr "[:upper:]" "[:lower:]" > "${DATADIR}"/cooking/cooking.preprocessed.txt
+fi
