5bbc47eb8c8786368f8d594a481235d60848f80b
Curtis Northcutt <curtis.northcutt@gmail.com>
2018-12-07 20:19:51 -0500

    Towards full coverage of util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -77,3 +77,33 @@ def test_print_noise_matrix():
         util.print_noise_matrix(noise_matrix)
     assert(True)
 
+
+# In[14]:
+
+
+def test_pu_f1():
+    s = [1, 1, 1, 0, 0, 0]
+    p = [1, 1, 1, 0, 0, 0]
+    assert(abs(util.estimate_pu_f1(s, p) - 1) < 1e-4)
+
+
+# In[22]:
+
+
+def test_value_counts_str():
+    r = util.value_counts(['a','b','a'])
+    assert(all(np.array([2, 1]) - r < 1e-4))
+
+
+# In[43]:
+
+
+def test_pu_remove_noise():
+    nm = np.array([
+        [0.9, 0.0, 0.0],
+        [0.0, 0.7, 0.4],
+        [0.1, 0.3, 0.6]
+    ])
+    r = util.remove_noise_from_class(nm, 0)
+    assert(np.all(r - nm < 1e-4))
+
