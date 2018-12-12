0c7aafd5afc2e4266c60035e973b50ccf7c675f9
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2018-12-12 00:58:49 -0500

    v0.0.6 - NO API changes. improved README Tutorials

--- cleanlab/version.py
+++ cleanlab/version.py
@@ -1,5 +1,6 @@
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
+# 0.0.6 - NO API changes. README updates. Examples added. Tutorials added.
 # 0.0.5 - Numerous small bug fixes, but not major API changes. 100% testing code coverage.
 # 0.0.4 - FIRST CROSS-PLATFORM WORKING VERSION OF CLEANLAB. Adding test support.
 # 0.0.3 - Adding working logo to README, pypi working
