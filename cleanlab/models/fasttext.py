f5b6a293681abb8b867dcae34624645909509fc6
Curtis Northcutt <curtis.northcutt@gmail.com>
2018-12-30 12:51:02 -0800

    Complete fefactor to use dataloader for large datasets in all methods.

--- cleanlab/models/fasttext.py
+++ cleanlab/models/fasttext.py
@@ -40,27 +40,53 @@ LABEL = '__label__'
 # In[ ]:
 
 
-def _get_labels_text(
+def data_loader(
     fn = None,
     indices = None, 
     label = LABEL,
+    batch_size = 1000,
 ):
-    '''Helper function for FastTextClassifier'''
-
-    if fn is None:
-        return [],[]
-
-    # Prepare data as a list of strings
-    with open(fn, 'rU') as f:
-        data = [z.strip() for z in f.readlines()]
-    # Split text data into a list of examples
-    data = [label+z.strip() for z in ''.join(data).split(label) if len(z) > 0]
-    if indices is not None and len(indices) < len(data):
-        # Only fetch indices provided by indices.
-        data = [data[i] for i in indices]
-    # Seperate labels and text
-    labels, text = [list(t) for t in zip(*(z.split(" ", 1) for z in data))]
-    return (labels, text)
+    '''Returns a generator, yielding two lists containing
+    [labels], [text]. Items are always returned in the 
+    order in the file, regardless if indices are provided.'''
+    
+    def _split_labels_and_text(batch):
+        l, t = [list(t) for t in zip(*(z.split(" ", 1) for z in batch))]
+        return l, t
+    
+    with open(fn, 'r') as f:
+        len_label = len(label)
+        idx = 0
+        batch_counter = 0
+        prev = f.readline()
+        batch = []
+        while True:
+            try:
+                line = f.readline()
+                if line[:len_label] == label or line == '':
+                    if indices is None or idx in indices:
+                        # Write out prev line and reset prev
+                        batch.append(prev)
+                        batch_counter += 1
+                    prev = ''
+                    idx += 1
+                    if batch_counter == batch_size:
+                        yield _split_labels_and_text(batch)
+                        # Reset batch
+                        batch_counter = 0
+                        batch = []
+                prev += line
+                if line == '':
+                    if len(batch) > 0:
+                        yield _split_labels_and_text(batch)
+                    break
+            except EOFError:
+                if indices is None or idx in indices:
+                    # Write out prev line and reset prev
+                    batch.append(prev)
+                    batch_counter += 1
+                    yield _split_labels_and_text(batch)
+                break
 
 
 # In[ ]:
@@ -79,6 +105,7 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
         del_intermediate_data = True,
         kwargs_train_supervised = {},
         p_at_k = 5,
+        batch_size = 1000,
         
     ):
         self.train_data_fn = train_data_fn
@@ -88,25 +115,22 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
         self.del_intermediate_data = del_intermediate_data
         self.kwargs_train_supervised = kwargs_train_supervised  
         self.p_at_k = p_at_k
+        self.batch_size = batch_size
         
         if labels is None:
-            #Find all class labels across the train and test set
-            labels = set(_get_labels_text(fn = train_data_fn)[0])
-            labels = labels.union(set(_get_labels_text(fn = test_data_fn)[0]))
+            #Find all class labels across the train and test set (if provided)
+            unique_labels = set([])
+            for labels, _ in data_loader(fn=train_data_fn, batch_size=batch_size):
+                unique_labels = unique_labels.union(set(labels))
+            if test_data_fn is not None:
+                for labels, _ in data_loader(fn=test_data_fn, batch_size=batch_size):
+                    unique_labels = unique_labels.union(set(labels))
         else:
-            # Prepend labels with label (e.g. '__label__'). 
-            labels = [label + str(l) for l in labels]
+            # Prepend labels with self.label token (e.g. '__label__'). 
+            unique_labels = [label + str(l) for l in labels]
         # Create maps: label strings <-> integers when label strings are used
-        labels = sorted(list(labels))
-        self.label2num = dict(zip(labels, range(len(labels))))
-        self.num2label = dict((y,x) for x,y in self.label2num.items())
-        
-        #Find all class labels across the train and test set
-        labels = set(_get_labels_text(fn = train_data_fn)[0])
-        labels = labels.union(set(_get_labels_text(fn = test_data_fn)[0]))
-        # Create maps: label strings <-> integers when label strings are used
-        labels = sorted(list(labels))
-        self.label2num = dict(zip(labels, range(len(labels))))
+        unique_labels = sorted(list(unique_labels))
+        self.label2num = dict(zip(unique_labels, range(len(unique_labels))))
         self.num2label = dict((y,x) for x,y in self.label2num.items())
     
     
@@ -118,35 +142,30 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
             self.masked_data_was_created = False
             return self.train_data_fn
         # Mask training data by data_indices
-        else:
-            # Read in training data as a list of strings
-            with open(self.train_data_fn, 'rU') as f:
-                data = f.readlines()
-            # Split into each training example
-            data = [self.label+z for z in ''.join(data).split(self.label) if len(z) > 0]
-            # If X indexes all training data, no need to rewrite the file.
-            if len(data_indices) == len(data):
-                self.masked_data_was_created = False
-                return self.train_data_fn
-            # Mask by data_indices
-            masked_data = [data[i] for i in data_indices]
+        else:            
             masked_fn = "fastTextClf_" + str(int(time.time())) + ".txt"
-            with open(masked_fn, 'w') as f:
-                f.writelines(masked_data)
+            # Read in training data one line at a time
+            with open(self.train_data_fn, 'rU') as f:
+                idx = 0
+                for line in f:
+                    # Mask by data_indices
+                    if idx in data_indices:
+                        with open(masked_fn, 'a+') as f:
+                            f.write(masked_data)
             self.masked_data_was_created = True
                 
         return masked_fn
     
     
     def _remove_masked_data(self, fn):
-        '''docstring'''
+        '''Deletes intermediate data files.'''
         
         if self.del_intermediate_data and self.masked_data_was_created:
             os.remove(fn)
     
     
     def fit(self, X = None, y = None, sample_weight = None):
-        '''docstring'''
+        '''Trains the fast text classifier.'''
         
         train_fn = self._create_train_data(data_indices = X)
         self.clf = train_supervised(train_fn, **self.kwargs_train_supervised)
@@ -154,16 +173,25 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
     
     
     def predict_proba(self, X = None, train_data = True, return_labels = False):
-        '''docstring'''
+        '''Produces a probability matrix with exmaples on rows and 
+        classes on columns, where each row sums to 1 and captures the
+        probability of the example belonging to each class.'''
         
         fn = self.train_data_fn if train_data else self.test_data_fn
-        gold_labels, text = _get_labels_text(fn = fn, indices = X)
-        pred = self.clf.predict(text = text, k = len(self.clf.get_labels()))
-        # Get p(s = k | x) matrix of shape (N x K) of pred probs for each example & class label
-        psx = [[p for _, p in sorted(list(zip(*l)), key=lambda x: x[0])] for l in list(zip(*pred))]
-        psx = np.array(psx)
+        psx_list = []
         if return_labels:
-            return (psx, np.array([self.label2num[y] for y in gold_labels]))
+            labels_list = []
+        for labels, text in data_loader(fn=fn, indices=X, batch_size=self.batch_size):
+            pred = self.clf.predict(text = text, k = len(self.clf.get_labels()))    
+            # Get p(s = k | x) matrix of shape (N x K) of pred probs for each example & class label
+            psx = [[p for _, p in sorted(list(zip(*l)), key=lambda x: x[0])] for l in list(zip(*pred))]
+            psx_list.append(np.array(psx))
+            if return_labels:
+                labels_list.append(labels)
+        psx = np.concatenate(psx_list, axis = 0) 
+        if return_labels:
+            gold_labels = [self.label2num[z] for l in labels_list for z in l]
+            return (psx, np.array(gold_labels))
         else:
             return psx
     
@@ -172,10 +200,18 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
         '''Predict labels of X'''
         
         fn = self.train_data_fn if train_data else self.test_data_fn
-        gold_labels, text = _get_labels_text(fn = fn, indices = X)
-        pred = np.array([self.label2num[z[0]] for z in self.clf.predict(text)[0]])
+        pred_list = []
         if return_labels:
-            return (pred, np.array([self.label2num[y] for y in gold_labels]))
+            labels_list = []
+        for labels, text in data_loader(fn=fn, indices=X, batch_size=self.batch_size):
+            pred = [self.label2num[z[0]] for z in self.clf.predict(text)[0]]
+            pred_list.append(pred)
+            if return_labels:
+                labels_list.append(labels)
+        pred = np.array([z for l in pred_list for z in l])
+        if return_labels:
+            gold_labels = [self.label2num[z] for l in labels_list for z in l]
+            return (pred, np.array(gold_labels))
         else:
             return pred
     
@@ -188,9 +224,22 @@ class FastTextClassifier(BaseEstimator): # Inherits sklearn base classifier
         if k is None:
             k = self.p_at_k
             
-        test_labels, text = _get_labels_text(fn = self.test_data_fn, indices = X)
-        y = test_labels if y is None else [self.num2label[z] for z in y]
-        apk = np.mean([y[i] in l for i, l in enumerate(self.clf.predict(text, k = k)[0])])
+        fn = self.test_data_fn
+        pred_list = []
+        if y is None:
+            labels_list = []
+        for labels, text in data_loader(fn=fn, indices=X, batch_size=self.batch_size):
+            pred = self.clf.predict(text, k = k)[0]
+            pred_list.append(pred)
+            if y is None:
+                labels_list.append(labels)
+        pred = np.array([z for l in pred_list for z in l])
+        if y is None:
+            y = [z for l in labels_list for z in l]
+        else:
+            y = [self.num2label[z] for z in y]
+            
+        apk = np.mean([y[i] in l for i, l in enumerate(pred)])
         
         return apk
 
