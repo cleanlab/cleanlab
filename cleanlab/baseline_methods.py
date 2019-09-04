6b6f8f6f5a8ce2ec4b51ce11704cedc4e44a89b9
Curtis Northcutt <curtis.northcutt@gmail.com>
2019-09-04 16:53:29 -0400

    Add baseline_methods to cleanlab package

---
+++ cleanlab/baseline_methods.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# Baseline methods
+# 
+# Contains baseline methods for estimating label errors.
+#
+# These methods ONLY WORK FOR SINGLE_LABEL (not multi-label)
+
+from __future__ import (
+    print_function, absolute_import, division, unicode_literals, with_statement
+)
+from cleanlab.util import confusion_matrix
+from cleanlab.pruning import get_noise_indices
+from cleanlab.latent_estimation import calibrate_confident_joint
+import numpy as np
+
+
+def baseline_argmax(psx, s):
+    '''This is the simplest baseline approach. Just consider 
+    anywhere argmax != s as a label error.
+
+    Parameters
+    ----------
+
+    s : np.array
+        A discrete vector of noisy labels, i.e. some labels may be erroneous.
+
+    psx : np.array (shape (N, K))
+        P(label=k|x) is a matrix with K (noisy) probabilities for each of the
+        N examples x. This is the probability distribution over all K classes,
+        for each example, regarding whether the example has label s==k P(s=k|x).
+        psx should have been computed using 3 (or higher) fold cross-validation.
+
+    Returns
+    -------
+        A boolean mask that is true if the example belong
+        to that index is label error..'''
+    
+    return np.argmax(psx, axis=1) != s
+
+
+def baseline_argmax_confusion_matrix(psx, s, calibrate=False):
+    '''This is a baseline approach. That uses the a confusion matrix
+    of argmax(psx) and s as the confident joint and then uses cleanlab
+    (confident learning) to find the label errors using this matrix.
+
+    Parameters
+    ----------
+
+    s : np.array
+        A discrete vector of noisy labels, i.e. some labels may be erroneous.
+
+    psx : np.array (shape (N, K))
+        P(label=k|x) is a matrix with K (noisy) probabilities for each of the
+        N examples x. This is the probability distribution over all K classes,
+        for each example, regarding whether the example has label s==k P(s=k|x).
+        psx should have been computed using 3 (or higher) fold cross-validation.
+
+    Returns
+    -------
+        A boolean mask that is true if the example belong
+        to that index is label error..'''
+
+    confident_joint = confusion_matrix(true=np.argmax(psx, axis=1), pred=s).T
+    if calibrate:
+        confident_joint = calibrate_confident_joint(confident_joint, s)
+    return get_noise_indices(s, psx, confident_joint=confident_joint)
+
+
+def baseline_argmax_calibrated_confusion_matrix(psx, s):
+    '''docstring is the same as baseline_argmax_confusion_matrix
+    Except in this method, we calibrate the confident joint created using
+    the confusion matrix before using cleanlab to find the label errors.'''
+
+    return baseline_argmax_confusion_matrix(psx, s, calibrate=True)
+    
