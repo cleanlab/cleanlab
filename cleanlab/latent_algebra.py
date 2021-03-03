40c907a895d65b43277b69292f87417f543cea33
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2021-03-03 18:07:13 -0500

    update for GNU GPL licensing

--- cleanlab/latent_algebra.py
+++ cleanlab/latent_algebra.py
@@ -1,5 +1,23 @@
 # coding: utf-8
 
+# Copyright (c) 2017-2050 Curtis G. Northcutt
+# This file is part of cleanlab.
+#
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+# This agreement applies to this version and all previous versions of cleanlab.
+
+
 # ## Latent Algebra
 # 
 # #### Contains mathematical functions relating the latent terms,
