e0648b0c286d46af2e619081044cdc1cf6b5cfe5
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2021-04-08 22:07:57 -0400

    Add license

--- cleanlab/models/cifar_cnn.py
+++ cleanlab/models/cifar_cnn.py
@@ -1,5 +1,22 @@
 # coding: utf-8
 
+# Copyright (c) 2017-2050 Curtis G. Northcutt
+# This file is part of cleanlab.
+#
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+# This agreement applies to this version and all previous versions of cleanlab.
+
 # ## A PyTorch CNN for training CIFAR-10 using Co-Teaching.
 # Code adapted from: https://github.com/bhanML/Co-teaching/blob/master/model.py
 # This code requires you have PyTorch installed
