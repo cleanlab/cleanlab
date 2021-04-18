1c815dee13988806cba544bd6b19bff3dd20bfc1
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2021-04-18 14:11:41 -0400

    Update for GNU GPL-3+ License.

--- setup.py
+++ setup.py
@@ -23,13 +23,13 @@ exec(open('cleanlab/version.py').read())
 setup(
     name='cleanlab',
     version=__version__,
-    license='MIT',
+    license='GPLv3+',
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    description = 'Find label errors in datasets, weak supervision, and learning with noisy labels. Works for all datasets and models.',
+    description = 'The standard package for machine learning with noisy labels and finding mislabeled data in Python.',
     url = 'https://github.com/cgnorthcutt/cleanlab',
     author = 'Curtis G. Northcutt',
-    author_email = 'cgn@mit.edu',
+    author_email = 'cgn@csail.mit.edu',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -38,15 +38,20 @@ setup(
       'Intended Audience :: Developers',
       'Intended Audience :: Education',
       'Intended Audience :: Science/Research',
+      'Intended Audience :: Information Technology',
+      'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+      'Natural Language :: English',
 
-      'License :: OSI Approved :: MIT License',
-
-      # We believe this package works will all versions, but we do not guarantee it!
+      # We believe this package works will these versions, but we do not guarantee it!
+      'Programming Language :: Python :: 2',
       'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
       'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
 
       'Programming Language :: Python',
       'Topic :: Software Development',
@@ -57,23 +62,25 @@ setup(
       'Topic :: Software Development',
       'Topic :: Software Development :: Libraries',
       'Topic :: Software Development :: Libraries :: Python Modules',
-
-      'Operating System :: Microsoft :: Windows',
-      'Operating System :: POSIX',
-      'Operating System :: Unix',
-      'Operating System :: MacOS',
     ],
-
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    
     # What does your project relate to?
     keywords='machine_learning denoising classification weak_supervision learning_with_noisy_labels unsupervised_learning',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['img', 'examples']),
+    
+    # Include cleanlab license file.    
+    include_package_data=True,
+    package_data={
+        "": ["LICENSE"],
+    }
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.11.3', 'scikit-learn>=0.18', 'scipy>=1.1.0', ],
+    install_requires=['numpy>=1.11.3', 'scikit-learn>=0.18', 'scipy>=1.1.0', 'tqdm>=4.53.0', ],
 )
