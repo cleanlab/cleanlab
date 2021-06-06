6eba9e344aa7741b6811fc9e19a0bece57098e0d
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2021-06-05 21:25:59 -0400

    add agpl-3 license

--- cleanlab/models/fasttext.py
+++ cleanlab/models/fasttext.py
@@ -1,4 +1,20 @@
-# coding: utf-8
+# Copyright (C) 2017-2050  Curtis G. Northcutt
+# This file is part of cleanlab.
+# 
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
+#
+# This agreement applies to this version and all previous versions of cleanlab.
 
 # Python 2 and 3 compatibility
 from __future__ import (
