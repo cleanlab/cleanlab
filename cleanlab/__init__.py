056b97193556a4610f4030d7ea9aaeb337203159
Curtis G. Northcutt <curtis.northcutt@gmail.com>
2022-02-17 13:48:10 -0800

    Remove polyplex (research) algorithms from cleanlab

--- cleanlab/__init__.py
+++ cleanlab/__init__.py
@@ -3,7 +3,6 @@ from . import classification
 from . import latent_algebra
 from . import count
 from . import noise_generation
-from . import polyplex
 from . import filter
 from . import util
 from . import baseline_methods
