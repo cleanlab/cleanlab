dc6fa956cb1c95293f2f65a6aebdf63c81d6e603
Anish Athalye <me@anishathalye.com>
2022-04-11 13:15:43 -0400

    Make a pass over docs

--- cleanlab/dataset.py
+++ cleanlab/dataset.py
@@ -15,7 +15,7 @@
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 
 
-"""Dataset Module
+"""
 Supports dataset-level and class-level automated quality, including finding which classes should
 be merged with other classes in your dataset, which classes should be removed,
 and which classes tend to be annotated most/least correctly overall.
@@ -43,9 +43,7 @@ def rank_classes_by_label_quality(
     Score values are unnormalized and may tend to be very small. What matters is their relative
     ranking across the classes.
 
-    Parameters
-    ----------
-    For parameter info, see the docstring of `dataset.find_overlapping_classes`
+    **Parameters**: For parameter info, see the docstring of :py:func:`find_overlapping_classes <cleanlab.dataset.find_overlapping_classes>`.
 
     Returns
     -------
@@ -54,25 +52,15 @@ def rank_classes_by_label_quality(
         "Overall Label Issues", "Inverse Label Noise Score", "Overall Label Quality Score" and a
         description of each below. Noise scores are between 0 and 1, where 0 implies no label issues
         in the class. The "Overall Label Quality Score" is also between 0 and 1 where 1 implies
-        perfect quality.
-        Columns:
-        * "Class Index"
-            The index of the class in 0, 1, ..., num_classes - 1.
-        * Overall Label Issues
-            count(given_label = k, true_label != k)
-            Estimated number of label issues in the class (usually the most accurate method).
-        * Inverse Label Issues
-            count(given_label != k, true_label = k)
-            Estimated number of times this class occurs in other classes in the dataset.
-        * Overall Label Noise Score
-            prob(true_label != k | given_label = k)
-            Estimated proportion of label issues in the class.
-        * Inverse Label Noise Score
-            prob(given_label != k | true_label = k)
-            Estimated proportion of times this class label occurs in other classes in the dataset.
-        * Overall Label Quality Score
-            p(given_label = k | true_label = k)
-            How accurate the labels are in each class as a conditional prob p(given=k | true=k).
+        perfect quality. Columns:
+
+        * *Class Index*: The index of the class in 0, 1, ..., K-1.
+        * *Overall Label Issues*: ``count(given_label = k, true_label != k)``, estimated number of label issues in the class (usually the most accurate method).
+        * *Inverse Label Issues*: ``count(given_label != k, true_label = k)``, estimated number of times this class occurs in other classes in the dataset.
+        * *Overall Label Noise Score*: ``prob(true_label != k | given_label = k)``, estimated proportion of label issues in the class.
+        * *Inverse Label Noise Score*: ``prob(given_label != k | true_label = k)``, estimated proportion of times this class label occurs in other classes in the dataset.
+        * *Overall Label Quality Score*: ``p(given_label = k | true_label = k)``, how accurate the labels are in each class as a conditional prob ``p(given=k | true=k)``.
+
         By default, the DataFrame is ordered by "Overall Label Noise Score" descending.
     """
 
@@ -119,93 +107,92 @@ def find_overlapping_classes(
     """Returns the classes that are often confused by machine learning model or data labelers.
     Consider merging the top pairs of classes returned by this method each into a single class.
     If the dataset is labeled by human annotators, consider clearly defining the
-    difference between the classes prior to have annotators label the data.
+    difference between the classes prior to having annotators label the data.
 
-    This method provides two scores in the pandas Data Frame that is returned:
-    * "Num Overlapping Examples" - The number of examples where the two classes overlap
-    * "Joint Probability" - "Num Overlapping Examples" / total number of examples in the dataset.
-    interchangeable and returns a dataframe with the classe and the joint probability score
-    between 0 and 1. Use this function to determine which classes in a dataset should be merged.
+    This method provides two scores in the Pandas DataFrame that is returned:
+
+    * **Num Overlapping Examples**: The number of examples where the two classes overlap
+    * **Joint Probability**: `(num overlapping examples / total number of examples in the dataset`).
 
     This method works by providing any one (and only one) of the following inputs:
-    1. labels and pred_probs, or
-    2. joint, or
-    3. confident_joint
-    but only provide **one of the above input options**, do not provide a combination.
 
-    The method uses the joint distribution of noisy and true labels to compute ontological issues
-    via the approach published in (Northcutt, Jiang, Chuang (2021) JAIR).
-    paper: https://arxiv.org/abs/1911.00068
+    1. `labels` and `pred_probs`, or
+    2. `joint`, or
+    3. `confident_joint`
+
+    Only provide **exactly one of the above input options**, do not provide a combination.
+
+    This method uses the joint distribution of noisy and true labels to compute ontological issues
+    via the approach published in `Northcutt et al., 2021 <https://jair.org/index.php/jair/article/view/12125>`_.
 
     This method measures how often the annotators confuse two classes.
     This method differs from just using a similarity matrix or confusion matrix. Instead, it works
-    even if the model that generated pred_probs in more confident in some classes than others
+    even if the model that generated `pred_probs` in more confident in some classes than others
     and has heterogeneity in average confidence across classes.
 
     Parameters
     ----------
-    labels : np.array (shape (num_examples, 1))
-        A discrete vector of noisy labels, i.e. some labels may be erroneous.
-        *Format requirements*: for dataset with K classes, labels must be in {0,1,...,K-1}.
+    labels : np.array, optional
+      An array of shape ``(N,)`` of noisy labels, i.e. some labels may be erroneous.
+      Elements must be in the set 0, 1, ..., K-1, where K is the number of classes.
 
-    pred_probs : np.array (shape (num_examples, num_classes))
-        P(label=k|x) is a matrix with K model-predicted probabilities.
-        Each row of this matrix corresponds to an example `x` and contains the model-predicted
-        probabilities that `x` belongs to each possible class.
-        The columns must be ordered such that these probabilities correspond to class 0,1,2,...
-        `pred_probs` should have been computed using 3 (or higher) fold cross-validation.
+    pred_probs : np.array, optional
+      An array of shape ``(N, K)`` of model-predicted probabilities,
+      ``P(label=k|x)``. Each row of this matrix corresponds
+      to an example `x` and contains the model-predicted probabilities that
+      `x` belongs to each possible class, for each of the K classes. The
+      columns must be ordered such that these probabilities correspond to
+      class 0, 1, ..., K-1. `pred_probs` should have been computed using 3 (or
+      higher) fold cross-validation.
 
-    asymmetric : bool (default: True)
-        If `asymmetric==True`, includes both pairs (class1, class2) and (class2, class1). Use this
-        for finding "is a" relationships where for example "class1 is a class2".
-        If `asymmetric==False`, the pair (class1, class2) will only be returned once and order is
-        arbitrary (internally this is just summing score(class1, class2) + score(class2, class1)).
+    asymmetric : bool, optional
+      If ``asymmetric=True``, includes both pairs (class1, class2) and (class2, class1). Use this
+      for finding "is a" relationships where for example "class1 is a class2".
+      If `asymmetric==False`, the pair (class1, class2) will only be returned once and order is
+      arbitrary (internally this is just summing score(class1, class2) + score(class2, class1)).
 
-    class_names : iterable<str>
+    class_names : Iterable[str]
         A list or other iterable of the string class names. The list should be in the order that
         matches the class indices. So if class 0 is 'dog' and class 1 is 'cat', then
-        `class_names = ['dog', 'cat'].
+        ``class_names = ['dog', 'cat']``.
 
-    num_examples : int (default: None)
-        The number of examples in the datasets, i.e. len(labels). You only need to provide this if
-        you use this function with the joint, e.g. `find_overlapping_classes(joint=joint)` otherwise
-        this is automatically inferred via `sum(confident_joint)` or `len(labels)`.
+    num_examples : int or None, optional
+        The number of examples in the datasets, i.e. ``len(labels)``. You only need to provide this if
+        you use this function with the joint, e.g. ``find_overlapping_classes(joint=joint)``, otherwise
+        this is automatically computed via ``sum(confident_joint)`` or ``len(labels)``.
 
-    joint : np.array<float> (shape (num_classes, num_classes))
-        Estimated joint distribution of the noisy labels and true labels. This takes the form of a
-        2-D square matrix (num_classes, num_classes) with all entries summing to 1 (valid
+    joint : np.array, optional
+        An array of shape ``(K, K)``, where K is the number of classes,
+        representing the estimated joint distribution of the noisy labels and
+        true labels. The sum of all entries in this matrix must be 1 (valid
         probability distribution). Each entry in the matrix captures the co-occurence joint
-        probability of a true label and a noisy label, i.e. p(noisy_label=i, true_label=j).
+        probability of a true label and a noisy label, i.e. ``p(noisy_label=i, true_label=j)``.
         **Important**. If you input the joint, you must also input `num_examples`.
 
-    confident_joint : np.array<int> (shape (num_examples, num_classes))
-        A K,K integer matrix of count(label=k, true_label=k). Estimates a confident subset of
-        the joint distribution of the noisy and true labels P_{labels,y}.
-        Each entry in the matrix contains the number of examples confidently
-        counted into every pair (label=j, true_label=k) classes.
+    confident_joint : np.array, optional
+      An array of shape ``(K, K)`` representing the confident joint, the matrix used for identifying label issues, which
+      estimates a confident subset of the joint distribution of the noisy and true labels, ``P_{noisy label, true label}``.
+      Entry ``(j, k)`` in the matrix is the number of examples confidently counted into the pair of ``(noisy label=j, true label=k)`` classes.
+      The `confident_joint` can be computed using :py:func:`count.compute_confident_joint <cleanlab.count.compute_confident_joint>`.
+      If not provided, it is computed from the given (noisy) `labels` and `pred_probs`.
 
-    multi_label : bool
-        If true, labels should be an iterable (e.g. list) of iterables, containing a
-        list of labels for each example, instead of just a single label.
-        The MAJOR DIFFERENCE in how this is calibrated versus single_label, is the total number of
-        errors considered is based on the number of labels, not the number of examples. So, the
-        calibrated confident_joint will sum to the number of total labels.
-        The multi-label setting supports classification tasks where an example has 1 or more labels.
-        Example of a multi-labeled `labels` input: [[0,1], [1], [0,2], [0,1,2], [0], [1], ...]
+    multi_label : bool, optional
+      If ``True``, labels should be an iterable (e.g. list) of iterables, containing a
+      list of labels for each example, instead of just a single label.
+      The multi-label setting supports classification tasks where an example has 1 or more labels.
+      Example of a multi-labeled `labels` input: ``[[0,1], [1], [0,2], [0,1,2], [0], [1], ...]``.
 
     Returns
     -------
     pd.DataFrame
         A Pandas DataFrame with columns "Class Index A", "Class Index B",
         "Num Overlapping Examples", "Joint Probability" and a description of each below.
-        * "Class Index A"
-            The index of a class in 0, 1, ..., num_classes - 1.
-        * "Class Index B"
-            The index of a different class (from Class A) in 0, 1, ..., num_classes - 1.
-        * Num Overlapping Examples
-            Estimated number of labels overlapping between the two classes.
-        * Joint Probability
-            The "Num Overlapping Examples" divided by the number of examples in the dataset.
+
+        * *Class Index A*: the index of a class in 0, 1, ..., K-1.
+        * *Class Index B*: the index of a different class (from Class A) in 0, 1, ..., K-1.
+        * *Num Overlapping Examples*: estimated number of labels overlapping between the two classes.
+        * *Joint Probability*: the *Num Overlapping Examples* divided by the number of examples in the dataset.
+
         By default, the DataFrame is ordered by "Joint Probability" descending.
     """
 
@@ -273,9 +260,7 @@ def overall_label_health_score(
     that, on average across all classes, about half of the label may have issues. Thus, a higher
     score implies higher quality labels, with 1 implying labels that have no issues.
 
-    Parameters
-    ----------
-    For parameter info, see the docstring of `dataset.find_overlapping_classes`
+    **Parameters**: For parameter info, see the docstring of :py:func:`find_overlapping_classes <cleanlab.dataset.find_overlapping_classes>`.
 
     Returns
     -------
@@ -315,20 +300,19 @@ def health_summary(
     confident_joint=None,
     multi_label=False,
 ):
-    """Prints a healthy summary of your datasets including results for powerful statistics like:
-    * the classes with the most and least label issues
-    * classes that overlap and could potentially be merged
-    * overall data label quality health score statistics for your dataset
+    """Prints a health summary of your datasets including results for useful statistics like:
 
-    Parameters
-    ----------
-    For parameter info, see the docstring of `dataset.find_overlapping_classes`
+    * The classes with the most and least label issues
+    * Classes that overlap and could potentially be merged
+    * Overall data label quality health score statistics for your dataset
+
+    **Parameters**: For parameter info, see the docstring of :py:func:`find_overlapping_classes <cleanlab.dataset.find_overlapping_classes>`.
 
     Returns
     -------
     health_score : float
-        A score between 0 and 1 where 1 implies the dataset has all estimated perfect labels.
-        A score of 0.5 implies that, on average, half of the dataset's label have estimated issues.
+        A score between 0 and 1, where 1 implies the dataset has all estimated perfect labels.
+        A score of 0.5 implies that, on average, half of the dataset's label have possible issues.
     """
 
     if joint is None:
