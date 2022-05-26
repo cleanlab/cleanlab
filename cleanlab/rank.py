a902eba711bda8bc4706f079ddf60bbbcd63c2e0
JohnsonKuan <johnson.h.kuan@gmail.com>
2022-05-26 11:31:29 -0700

    Update docstring in ensemble label quality scoring function

--- cleanlab/rank.py
+++ cleanlab/rank.py
@@ -310,7 +310,7 @@ def get_label_quality_ensemble_scores(
     # This weighting scheme performs search of t in log_loss_search_T_values for "best" log loss
     if weight_ensemble_members_by == "log_loss_search":
 
-        # Initialize
+        # Initialize variables for log loss search
         pred_probs_avg_log_loss_weighted = None
         neg_log_loss_weights = None
         best_eval_log_loss = float("inf")
