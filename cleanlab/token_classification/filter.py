1a918e95a09e44dc5ddbff4d7c4ff707f78832f3
Eric Wang <ericw@eric.mit.edu>
2022-08-09 15:43:09 -0400

    add filter.py

---
+++ cleanlab/token_classification/filter.py
@@ -0,0 +1,15 @@
+from cleanlab.filter import find_label_issues as find_label_issues_main 
+
+def find_label_issues(labels, pred_probs, return_indices_ranked_by='self_confidence'): 
+    labels_flatten = [l for label in labels for l in label] 
+    pred_probs_flatten = [pred for pred_prob in pred_probs for pred in pred_prob] 
+    pred_probs_flatten = np.array(pred_probs_flatten) 
+    
+    issues = find_label_issues_main(labels, pred_probs, return_indices_ranked_by=return_indices_ranked_by) 
+    
+    lengths = [len(label) for label in labels] 
+    mapping = [[(i, j) for j in range(length)] for i, length in enumerate(lengths)] 
+    mapping = [index for indicies in mapping for index in indicies] 
+    
+    issues = [mapping[issue] for issue in issues] 
+    return issues 
