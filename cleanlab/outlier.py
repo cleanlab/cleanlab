4b28d4372eb03305faafb47c23d7582613299a5e
Ulyana <ulyana@cleanlab.ai>
2022-08-26 16:36:43 -0700

    Added logic tests for function

--- cleanlab/outlier.py
+++ cleanlab/outlier.py
@@ -23,13 +23,11 @@ from cleanlab.rank import get_ood_scores, get_outlier_scores
 
 
 class OutOfDistribution:
-    OUTLIER_PARAMS = ["features", "k", "t"]
-    OOD_PARAMS = ["labels", "adjust_pred_probs", "method"]
+    OUTLIER_PARAMS = tuple(("k", "t"))
+    OOD_PARAMS = tuple(("labels", "adjust_pred_probs", "method"))
     DEFAULT_PARAM_DICT = {
-        "features": None,  # outlier param
         "k": None,  # outlier param
         "t": 1,  # outlier param
-        "labels": None,  # ood param
         "adjust_pred_probs": True,  # ood param
         "method": "entropy",  # ood param
     }
@@ -46,7 +44,7 @@ class OutOfDistribution:
         pred_probs: Optional[np.ndarray] = None,
         labels: Optional[np.ndarray] = None,
         params: dict = None,
-        verbose=True,
+        verbose: bool = True,
     ):
 
         scores = self._shared_fit(
@@ -72,20 +70,15 @@ class OutOfDistribution:
         pred_probs: Optional[np.ndarray] = None,
         labels: Optional[np.ndarray] = None,
         params: dict = None,
-        verbose=True,
+        verbose: bool = True,
     ):
+        _ = self._shared_fit(features=features, pred_probs=pred_probs, labels=labels, params=params)
 
-        _ = self._shared_fit(
-            features=features, pred_probs=pred_probs, labels=labels, params=params
-        )  # some of below code belongs in def of _shared_fit() instead
-
-        if features is not None:
-            # fit to outlier scores
+        if features is not None:  # fit to outlier scores
             if verbose:
                 print("Fitting OOD object based on provided features ...")
 
-        if pred_probs is not None:
-            # fit to ood scores
+        if pred_probs is not None:  # fit to ood scores
             if verbose:
                 print("Fitting OOD object based on provided pred_probs ...")
 
@@ -98,35 +91,52 @@ class OutOfDistribution:
         if features is not None:
             if self.knn is None:
                 raise ValueError(
-                    f"OOD Object needs to be fit first. Call fit() or fit_scores() before this function."
+                    f"OOD Object needs to be fit on features first. Call fit() or fit_scores() before this function."
                 )
             else:
                 params = self._get_params(self.OUTLIER_PARAMS)  # get params specific to outliers
                 scores = get_outlier_scores(features, self.knn, **params, return_estimator=False)
 
         if pred_probs is not None:
-            if self.confident_thresholds is None and self.adjust_pred_probs is not None:
+            if self.confident_thresholds is None and self.params["adjust_pred_probs"]:
                 raise ValueError(
-                    f"OOD Object needs to be fit first. Call fit() or fit_scores() with proper params before this function."
+                    f"OOD Object needs to be fit on pred_probs with param adjust_pred_probs=True first. Call fit() or "
+                    f"fit_scores() before this function. "
                 )
             else:
-                params = self._get_params(self.OOD_params)  # get params specific to outliers
-                scores = get_outlier_scores(features, self.knn, **params, return_estimator=False)
+                params = self._get_params(self.OOD_PARAMS)  # get params specific to outliers
+                scores = get_ood_scores(
+                    pred_probs,
+                    confident_thresholds=self.confident_thresholds,
+                    **params,
+                    return_thresholds=False,
+                )
 
         return scores
 
     def _get_params(self, param_keys):
         return {k: v for k, v in self.params.items() if k in param_keys}
 
-    def _assert_valid_inputs(self, pred_probs, features):
-        if features is not None and pred_probs is not None:
-            raise ValueError(
-                f"Cannot fit to object to features and pred_probs. Pass in either one or the other."
-            )
+    def _get_invalid_params(self, params, param_keys):
+        if params is None:
+            return []
+        return list(set(params.keys()).difference(set(param_keys)))
 
+    def _assert_valid_inputs(self, features, pred_probs):
         if features is None and pred_probs is None:
             raise ValueError(
                 f"Not enough information to compute scores. Pass in either features or pred_probs."
+            )
+
+        if features is not None and pred_probs is not None:
+            raise ValueError(
+                f"Cannot fit to object to both features and pred_probs. Pass in either one or the other."
+            )
+
+        if features is not None and len(features.shape) != 2:
+            raise ValueError(
+                f"Feature array needs to be of shape (N, M), where N is the number of examples and M is the "
+                f"number of features used to represent each example. "
             )
 
     def _shared_fit(
@@ -136,40 +146,48 @@ class OutOfDistribution:
         pred_probs: Optional[np.ndarray] = None,
         labels: Optional[np.ndarray] = None,
         params: dict = None,
+        verbose: bool = True,
     ):
-
+        # Assert valid inputs and params
         self._assert_valid_inputs(features, pred_probs)
-        self.params = self.DEFAULT_PARAM_DICT
 
+        # Update params
+        self.params = self.DEFAULT_PARAM_DICT
         if params is not None:
             self.params = {**self.params, **params}
 
         if features is not None:
             # raise Error if they passed in invalid params keys for using features.
-            if self.OOD_PARAMS in params.keys():
+            ood_params = self._get_invalid_params(params, self.OUTLIER_PARAMS)
+            if len(ood_params) > 0:
                 raise ValueError(
-                    f"Passed in params dict can only contain {self.OUTLIER_PARAMS}. Remove {list(set(params.keys()) & set(self.OOD_PARAMS))} from params dict."
+                    f"If fit with features, passed in params dict can only contain {self.OUTLIER_PARAMS}. Remove {ood_params} from params dict."
                 )
-
             # get outlier scores
-            if self.verbose:
+            if verbose:
                 print("Computing outlier scores based on provided features ...")
-            scores, knn = get_outlier_scores(features, **params, return_estimator=True)
+            scores, knn = get_outlier_scores(
+                features, **self._get_params(self.OUTLIER_PARAMS), return_estimator=True
+            )
             self.knn = knn  # save estimator
 
         if pred_probs is not None:
             # raise Error if they passed in invalid params keys for using pred_probs.
-            if self.OUTLIER_PARAMS in params.keys():
+            outlier_params = self._get_invalid_params(params, self.OOD_PARAMS)
+            if len(outlier_params) > 0:
                 raise ValueError(
-                    f"Passed in params dict can only contain {self.OOD_PARAMS}. Remove {list(set(params.keys()) & set(self.OUTLIER_PARAMS))} from params dict."
+                    f"If fit with pred_probs, passed in params dict can only contain {self.OOD_PARAMS}. Remove {outlier_params} from params dict."
                 )
 
             # get ood scores
-            if self.verbose:
+            if verbose:
                 print("Computing ood scores based on provided features ...")
             scores, confident_thresholds = get_ood_scores(
-                pred_probs, labels=labels, **params, return_estimator=True
+                pred_probs,
+                labels=labels,
+                **self._get_params(self.OOD_PARAMS),
+                return_thresholds=True,
             )
-            self.confident_thresholds = confident_thresholds  # save estimator
+            self.confident_thresholds = confident_thresholds
 
         return scores
--- tests/test_outlier.py
+++ tests/test_outlier.py
@@ -13,3 +13,274 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+import pytest
+from cleanlab.benchmarking.noise_generation import generate_noise_matrix_from_trace
+from cleanlab.benchmarking.noise_generation import generate_noisy_labels
+from cleanlab import count
+from cleanlab.outlier import OutOfDistribution
+
+
+def make_data(
+    means=[[3, 2], [7, 7], [0, 8]],
+    covs=[[[5, -1.5], [-1.5, 1]], [[1, 0.5], [0.5, 4]], [[5, 1], [1, 5]]],
+    sizes=[80, 40, 40],
+    avg_trace=0.8,
+    seed=1,  # set to None for non-reproducible randomness
+):
+    np.random.seed(seed=seed)
+
+    m = len(means)  # number of classes
+    n = sum(sizes)
+    local_data = []
+    labels = []
+    test_data = []
+    test_labels = []
+
+    for idx in range(m):
+        local_data.append(
+            np.random.multivariate_normal(mean=means[idx], cov=covs[idx], size=sizes[idx])
+        )
+        test_data.append(
+            np.random.multivariate_normal(mean=means[idx], cov=covs[idx], size=sizes[idx])
+        )
+        labels.append(np.array([idx for i in range(sizes[idx])]))
+        test_labels.append(np.array([idx for i in range(sizes[idx])]))
+    X_train = np.vstack(local_data)
+    true_labels_train = np.hstack(labels)
+    X_test = np.vstack(test_data)
+    true_labels_test = np.hstack(test_labels)
+
+    # Compute p(true_label=k)
+    py = np.bincount(true_labels_train) / float(len(true_labels_train))
+
+    noise_matrix = generate_noise_matrix_from_trace(
+        m,
+        trace=avg_trace * m,
+        py=py,
+        valid_noise_matrix=True,
+        seed=seed,
+    )
+
+    # Generate our noisy labels using the noise_matrix.
+    s = generate_noisy_labels(true_labels_train, noise_matrix)
+    ps = np.bincount(s) / float(len(s))
+
+    # Compute inverse noise matrix
+    inv = count.compute_inv_noise_matrix(py, noise_matrix, ps=ps)
+
+    # Estimate pred_probs
+    latent = count.estimate_py_noise_matrices_and_cv_pred_proba(
+        X=X_train,
+        labels=s,
+        cv_n_folds=3,
+    )
+
+    label_errors_mask = s != true_labels_train
+
+    return {
+        "X_train": X_train,
+        "true_labels_train": true_labels_train,
+        "X_test": X_test,
+        "true_labels_test": true_labels_test,
+        "labels": s,
+        "label_errors_mask": label_errors_mask,
+        "ps": ps,
+        "py": py,
+        "noise_matrix": noise_matrix,
+        "inverse_noise_matrix": inv,
+        "est_py": latent[0],
+        "est_nm": latent[1],
+        "est_inv": latent[2],
+        "cj": latent[3],
+        "pred_probs": latent[4],
+        "m": m,
+        "n": n,
+    }
+
+
+# Global to be used by all test methods. Only compute this once for speed.
+data = make_data()
+
+
+def test_wrong_info_assert_valid_inputs():
+    features = data["X_train"]
+    pred_probs = data["pred_probs"]
+
+    OOD = OutOfDistribution()
+
+    # TESTING: _assert_valid_inputs() asserts correct errors in fit
+    try:
+        OOD.fit()
+    except Exception as e:
+        assert "Not enough information to compute scores" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit()
+    try:
+        OOD.fit(features=features, pred_probs=pred_probs)
+    except Exception as e:
+        assert "Cannot fit to object to both features and pred_probs" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit(features=features, pred_probs=pred_probs)
+
+    OOD = OutOfDistribution()
+
+    features_flat = np.ravel(features)
+    features_extra_dim = features[np.newaxis]
+    try:
+        OOD.fit(features=features_flat)
+    except Exception as e:
+        assert "array needs to be of shape (N, M)" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit(features=features_flat)
+    try:
+        OOD.fit(features=features_extra_dim)
+    except Exception as e:
+        assert "array needs to be of shape (N, M)" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit(features=features_extra_dim)
+
+    # TODO: DO WE NEED TO TESTING: _assert_valid_inputs() asserts correct errors in score?
+
+
+def test_wrong_info_fit_ood():
+    features = data["X_train"]
+    pred_probs = data["pred_probs"]
+    labels = data["labels"]
+
+    OOD = OutOfDistribution()
+
+    ##### SHARED FIT wrong info
+    # TESTING: wrong params
+    with pytest.raises(ValueError) as e:  # testing wrong params
+        OOD.fit(features=features, params={"strange_param": -1})
+    with pytest.raises(ValueError) as e:  # testing wrong params
+        OOD.fit(pred_probs=pred_probs, params={"strange_param": -1})
+
+    # testing OOD param in fit for outlier detection
+    mixed_params_dict = {"k": 10, "adjust_pred_probs": True}
+    try:
+        OOD.fit(features=features, params=mixed_params_dict)
+    except Exception as e:
+        assert "with features, passed in params dict can only contain" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit(features=features, params=mixed_params_dict)
+
+    # testing outlier param in fit for OOD detection
+    try:
+        OOD.fit(pred_probs=pred_probs, params=mixed_params_dict)
+    except Exception as e:
+        assert "with pred_probs, passed in params dict can only contain" in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.fit(features=features, params=mixed_params_dict)
+
+    #### SCORE wrong info
+
+    # TESTING: calling score before any fitting
+    try:
+        OOD.score(features=features)
+    except Exception as e:
+        assert "OOD Object needs to be fit on features first." in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD.score(features=features)
+
+    try:
+        OOD.score(pred_probs=pred_probs)
+    except Exception as e:
+        assert (
+            "OOD Object needs to be fit on pred_probs with param adjust_pred_probs=True first."
+            in str(e)
+        )
+        with pytest.raises(ValueError) as e:
+            OOD.score(pred_probs=pred_probs)
+
+    # TESTING: calling scoring with opposite fitting
+    OOD_outlier = OutOfDistribution()
+    OOD_outlier.fit(features=features)
+    try:
+        OOD_outlier.score(pred_probs=pred_probs)
+    except Exception as e:
+        assert (
+            "OOD Object needs to be fit on pred_probs with param adjust_pred_probs=True first."
+            in str(e)
+        )
+        with pytest.raises(ValueError) as e:
+            OOD_outlier.score(pred_probs=pred_probs)
+
+    OOD_ood = OutOfDistribution()
+    OOD_ood.fit(pred_probs=pred_probs, labels=labels)
+    try:
+        OOD_ood.score(features=features)
+    except Exception as e:
+        assert "OOD Object needs to be fit on features first." in str(e)
+        with pytest.raises(ValueError) as e:
+            OOD_ood.score(features=features)
+
+    # # TODO FIX THIS! get_ood_thresholds returns 1-2 variables this does not work
+    # # TESTING: calling ood after fitting but without adjust_pred_probs
+    # OOD_ood = OutOfDistribution()\
+    # OOD_ood.fit(pred_probs=pred_probs, params={"adjust_pred_probs": False})
+    # try:
+    #     OOD_ood.score(features=features)
+    # except Exception as e:
+    #     assert "OOD Object needs to be fit on pred_probs with param adjust_pred_probs=True first." in str(e)
+    #     with pytest.raises(ValueError) as e:
+    #         OOD_ood.score(features=features)
+
+
+def test_params_logic():
+    features = data["X_train"]
+    pred_probs = data["pred_probs"]
+
+    OOD = OutOfDistribution()
+
+    # TESTING: params dict is a copy
+    params_dict = {"k": 10, "t": 5}
+    OOD.fit(features=features, params=params_dict)
+    ood_params = OOD.params
+    params_dict = params_dict.update({"k": 20})
+    assert ood_params == OOD.params
+
+    # test calling functions with different params performs differently
+
+
+def test_class_public_func():
+    features = data["X_test"]
+    pred_probs = data["pred_probs"]
+    labels = data["true_labels_test"]
+
+    #### TESTING FIT:
+    OOD_ood = OutOfDistribution()
+    OOD_ood.fit(pred_probs=pred_probs, labels=labels)
+
+    OOD_outlier = OutOfDistribution()
+    OOD_outlier.fit(features=features)
+
+    assert OOD_ood.confident_thresholds is not None and OOD_ood.knn is None
+    assert OOD_outlier.knn is not None and OOD_outlier.confident_thresholds is None
+    assert OOD_ood.params is not None and OOD_outlier.params is not None
+
+    #### TESTING SCORE
+    ood_score = OOD_ood.score(pred_probs=pred_probs)
+    outlier_score = OOD_outlier.score(features=features)
+
+    assert ood_score is not None and outlier_score is not None
+    assert np.sum(ood_score) != np.sum(outlier_score)
+
+    #### TESTING FIT SCORE
+    OOD_ood_fs = OutOfDistribution()
+    ood_score_fs = OOD_ood_fs.fit_score(pred_probs=pred_probs, labels=labels)
+    OOD_outlier_fs = OutOfDistribution()
+    outlier_score_fs = OOD_outlier_fs.fit_score(features=features)
+
+    assert OOD_ood_fs.confident_thresholds is not None and OOD_ood_fs.knn is None
+    assert (OOD_ood_fs.confident_thresholds == OOD_ood.confident_thresholds).all()
+
+    assert OOD_outlier.knn is not None and OOD_outlier.confident_thresholds is None
+
+    assert OOD_ood.params == OOD_ood_fs.params and OOD_outlier.params == OOD_outlier_fs.params
+    assert ood_score_fs is not None and outlier_score_fs is not None
+
+    assert np.sum(outlier_score_fs) - np.sum(outlier_score) < 1  # scores are similar
+    assert np.sum(ood_score_fs) - np.sum(ood_score) < 1  # scores are similar
