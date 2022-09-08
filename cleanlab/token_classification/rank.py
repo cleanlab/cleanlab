7694180dc3239945e5e6cb1d008ea9a7ad6f10c3
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2022-09-08 00:03:41 -0700

    header for token_classification.rank

--- cleanlab/token_classification/rank.py
+++ cleanlab/token_classification/rank.py
@@ -1,7 +1,28 @@
+# Copyright (C) 2017-2022  Cleanlab Inc.
+# This file is part of cleanlab.
+#
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Methods to rank and score sentences in a token classification dataset (text data), based on how likely they are to contain label errors.
+"""
+
 import pandas as pd
 import numpy as np
-from cleanlab.rank import get_label_quality_scores as main_get_label_quality_scores
 from typing import List, Optional, Union, Tuple
+
+from cleanlab.rank import get_label_quality_scores as main_get_label_quality_scores
 
 
 def softmin_sentence_score(token_scores: List[np.ndarray], temperature: float = 0.05) -> np.ndarray:
