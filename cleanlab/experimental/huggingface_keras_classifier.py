2f9c8d74887da2b8301a42a2a10d3a4efb38cdf1
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2022-09-08 00:14:33 -0700

    header for huggingfacekerasclassifier

--- cleanlab/experimental/huggingface_keras_classifier.py
+++ cleanlab/experimental/huggingface_keras_classifier.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2017-2022  Cleanlab Inc.
+# This file is part of cleanlab.
+#
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 A Scikeras classifier (adapted for fine-tuning HuggingFace pretrained Transformer models) which
 can be used for finding label issues in any text datasets.
