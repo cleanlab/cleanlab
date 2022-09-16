545aa6d4008f95afdb69989e37e1968b44f10109
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2022-09-16 16:11:47 -0400

    black formatting

--- cleanlab/internal/validation.py
+++ cleanlab/internal/validation.py
@@ -209,7 +209,9 @@ def labels_to_array(y: Union[LabelLike, np.generic]) -> np.ndarray:
             )
 
 
-def assert_valid_inputs_multiannotator(labels_multiannotator: Union[pd.DataFrame, np.ndarray], pred_probs: np.ndarray) -> None:
+def assert_valid_inputs_multiannotator(
+    labels_multiannotator: Union[pd.DataFrame, np.ndarray], pred_probs: np.ndarray
+) -> None:
     # Raise error if number of classes in labels_multiannoator does not match number of classes in pred_probs
     if pred_probs is not None:
         num_classes = get_num_classes(pred_probs=pred_probs)
