e6ebae955cd444157a7ee29848d1d7bcd86502ae
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2022-09-16 15:53:21 -0700

    Revert "Fix details disclosure elements in docs (#456)"
    This reverts commit d8cc2e50566e90dcfb9aff118a41610d4be070ee.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -230,8 +230,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_css_files = ["css/custom.css"]
-
 html_sidebars = {
     "**": [
         "brand.html",
