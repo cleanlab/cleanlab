2aacedbd5f6221b0c7f27a68a0c2bdd38e8069e1
Ethan <ethanotran@gmail.com>
2022-09-19 19:17:10 -0700

    updated label_quality_utils.py and rebuilt the doc

--- cleanlab/internal/label_quality_utils.py
+++ cleanlab/internal/label_quality_utils.py
@@ -96,17 +96,17 @@ def get_normalized_entropy(pred_probs: np.ndarray, min_allowed_prob: float = 1e-
 
     Parameters
     ----------
-    pred_probs : np.ndarray (shape (N, K))
+    pred_probs : np.ndarray
       Each row of this matrix corresponds to an example x and contains the model-predicted
       probabilities that x belongs to each possible class: P(label=k|x)
 
-    min_allowed_prob : float, default=1e-6
+    min_allowed_prob : 
       Minimum allowed probability value. Entries of `pred_probs` below this value will be clipped to this value.
       Ensures entropy remains well-behaved even when `pred_probs` contains zeros.
 
     Returns
     -------
-    entropy : np.ndarray (float)
+    entropy : np.ndarray
       Each element is the normalized entropy of the corresponding row of ``pred_probs``.
     """
 
