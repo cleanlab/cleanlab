28446e949eeb46c3e8debf719d1381d90ca1ecc4
Elías Snorrason <eliassno@gmail.com>
2022-09-22 21:05:12 +0000

    Pass docs versions as context variables to jinja (#482)
    This replaces the fix in #480.

--- docs/source/_templates/versioning.js
+++ docs/source/_templates/versioning.js
@@ -2,10 +2,3 @@ var Version = {
   version_number: "placeholder_version_number",
   commit_hash: "placeholder_commit_hash",
 };
-
-// Add new version tags here before a release 
-var ReleaseVersions = [
-  "2.1.0",
-  "2.0.0",
-  "1.0.1",
-]
