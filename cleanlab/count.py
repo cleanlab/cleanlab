a10759fc53f71e440fb9fb9caf472a3227680f44
Aditya Thyagarajan <aditya1593@icloud.com>
2022-10-14 23:15:41 +0530

    Update cleanlab/count.py
    Co-authored-by: Elías Snorrason <eliassno@gmail.com>

--- cleanlab/count.py
+++ cleanlab/count.py
@@ -605,7 +605,7 @@ def _compute_confident_joint_multi_label(
     Returns
     -------
     confident_joint_counts : np.ndarray
-      An array of shape ``(num_classes,2, 2)`` representing the confident joint, the matrix used for identifying label issues, which
+      An array of shape ``(K, 2, 2)`` representing the confident joint, the matrix used for identifying label issues, which
       estimates a confident subset of the joint distribution of the noisy and true labels, ``P_{noisy label, true label}``.
       Entry ``(c, j, k)`` in the matrix is the number of examples in a one-vs-rest class confidently counted into the pair of ``(class c, noisy label=j, true label=k)`` classes.
 
