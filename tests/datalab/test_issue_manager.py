110610d80fa6e08a21cfd58c8ae758072809b431
Elías Snorrason <eliassno@gmail.com>
2022-12-20 19:06:02 +0000

    test(datalab): 🔥 remove TestIssueManager test class

--- tests/datalab/test_issue_manager.py
+++ tests/datalab/test_issue_manager.py
@@ -5,37 +5,6 @@ import pytest
 from cleanlab.experimental.datalab.issue_manager import LabelIssueManager
 
 
-@pytest.mark.parametrize(
-    "issue_manager_class",
-    [LabelIssueManager],
-    ids=["LabelIssueManager"],
-)
-class TestIssueManager:
-    @pytest.fixture
-    def issue_manager(self, lab, issue_manager_class):
-        return issue_manager_class(datalab=lab)
-
-    def test_init(self, lab, issue_manager_class):
-        """Test that the init method works."""
-        issue_manager = issue_manager_class(datalab=lab)
-        assert issue_manager.datalab == lab
-
-    def test_find_issues(self, pred_probs, issue_manager):
-        """Test that the find_issues method works."""
-        issue_manager.find_issues(pred_probs=pred_probs)
-        issues, summary, info = issue_manager.issues, issue_manager.summary, issue_manager.info
-        assert isinstance(issues, pd.DataFrame), "Issues should be a dataframe"
-        is_issue_key = "_".join(["is", issue_manager.issue_key, "issue"])
-        assert is_issue_key in issues.columns, "Issues should have an is_<issue_key>_issue column"
-
-        assert isinstance(summary, pd.DataFrame), "Summary should be a dataframe"
-        assert all(
-            [col in summary.columns for col in ["issue_type", "score"]]
-        ), "Summary should have issue_type and score columns"
-
-        assert isinstance(info, dict), "Info should be a dict"
-
-
 class TestLabelIssueManager:
     @pytest.fixture
     def issue_manager(self, lab):
