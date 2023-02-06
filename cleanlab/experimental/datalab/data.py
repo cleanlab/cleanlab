ebbb0be7569c40061b766a74d148bcab4581a62a
Elías Snorrason <eliassno@gmail.com>
2023-02-06 14:35:55 +0000

    refactor(datalab): 🥅 update methods for loading datasets
    define errors related to data loading

--- cleanlab/experimental/datalab/data.py
+++ cleanlab/experimental/datalab/data.py
@@ -15,7 +15,8 @@
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 """Module for class and functions that hold and validate datasets that are loaded into DataLab."""
 
-from typing import List, Mapping, Tuple, Union, cast
+import os
+from typing import Any, Callable, Dict, List, Mapping, Tuple, Union, cast, TYPE_CHECKING
 
 import datasets
 import numpy as np
@@ -23,6 +24,54 @@ import pandas as pd
 from datasets.arrow_dataset import Dataset
 
 from cleanlab.internal.validation import labels_to_array
+
+
+if TYPE_CHECKING:  # pragma: no cover
+    DatasetLike = Union[Dataset, pd.DataFrame, Dict[str, Any], List[Dict[str, Any]], str]
+
+
+class DataFormatError(ValueError):
+    """Exception raised when the data is not in a supported format."""
+
+    def __init__(self, data: Any):
+        self.data = data
+        message = (
+            f"Unsupported data type: {type(data)}\n"
+            "Supported types: "
+            "datasets.Dataset, pandas.DataFrame, dict, list, str"
+        )
+        super().__init__(message)
+
+
+class DatasetDictError(ValueError):
+    """Exception raised when a DatasetDict is passed to DataLab.
+
+    Usually, this means that a dataset identifier was passed to DataLab, but
+    the dataset is a DatasetDict, which contains multiple splits of the dataset.
+
+    """
+
+    def __init__(self):
+        message = (
+            "Please pass a single dataset, not a DatasetDict. "
+            "Try specifying a split, e.g. `dataset = load_dataset('dataset', split='train')` "
+            "then pass `dataset` to DataLab."
+        )
+        super().__init__(message)
+
+
+class DatasetLoadError(ValueError):
+    """Exception raised when a dataset cannot be loaded.
+
+    Parameters
+    ----------
+    dataset_type: type
+        The type of dataset that failed to load.
+    """
+
+    def __init__(self, dataset_type: type):
+        message = f"Failed to load dataset from {dataset_type}.\n"
+        super().__init__(message)
 
 
 class Data:
@@ -35,9 +84,9 @@ class Data:
 
     Parameters
     ----------
-    data : Dataset
+    data :
         Dataset to be used for DataLab.
-        Other formats are supported, but will be converted to a Dataset object.
+        Several formats are supported, but will be converted to a Dataset object.
 
         Supported formats:
             - datasets.Dataset
@@ -59,57 +108,28 @@ class Data:
         Name of the label column in the dataset.
     """
 
-    def __init__(self, data: Dataset, label_name: Union[str, List[str]]) -> None:
+    def __init__(self, data: "DatasetLike", label_name: Union[str, List[str]]) -> None:
         self._validate_data(data)
-        self._validate_data_and_labels(data, label_name)
         self._data = self._load_data(data)
-        self._data_hash = hash(data)
+        self._validate_data_and_labels(self._data, label_name)
+        self._data_hash = hash(self._data)
         self._data.set_format(type="numpy")
-        self._labels, self._label_map = _extract_labels(data, label_name)
+        self._labels, self._label_map = _extract_labels(self._data, label_name)
         self._label_name = label_name
 
-    def _load_data(self, data) -> datasets.Dataset:
+    def _load_data(self, data: "DatasetLike") -> Dataset:
         """Checks the type of dataset and uses the correct loader method and
         assigns the result to the data attribute."""
-        if isinstance(data, datasets.Dataset):
-            return data
-        if isinstance(data, pd.DataFrame):
-            return datasets.Dataset.from_pandas(data)
-
-        if isinstance(data, dict):
-            try:
-                return datasets.Dataset.from_dict(data)
-            except Exception as e:
-                raise ValueError(
-                    "Failed to load data from dictionary. "
-                    "Ensure that it is a mapping from strings "
-                    "to arrays or lists of equal length.\n"
-                    f"Error: {e}"
-                ) from e
-        if isinstance(data, list):
-            try:
-                return datasets.Dataset.from_list(data)
-            except Exception as e:
-                raise ValueError(
-                    "Failed to load data from list. "
-                    "Ensure that it is a list of dictionaries "
-                    "with the same keys.\n"
-                    f"Error: {e}"
-                ) from e
-
-        if isinstance(data, str):
-            # Check if it's a path to a local file
-            import os
-
-            if os.path.exists(data):
-                if data.endswith(".txt"):
-                    return datasets.Dataset.from_text(data)
-                if data.endswith(".csv"):
-                    return datasets.Dataset.from_csv(data)
-                if data.endswith(".json"):
-                    return datasets.Dataset.from_json(data)
-
-            return datasets.load_dataset(data)
+        dataset_factory_map: Dict[type, Callable[..., Dataset]] = {
+            Dataset: lambda x: x,
+            pd.DataFrame: Dataset.from_pandas,
+            dict: self._load_dataset_from_dict,
+            list: self._load_dataset_from_list,
+            str: self._load_dataset_from_string,
+        }
+        if not isinstance(data, tuple(dataset_factory_map.keys())):
+            raise DataFormatError(data)
+        return dataset_factory_map[type(data)](data)
 
     def __len__(self) -> int:
         return len(self._data)
@@ -135,12 +155,9 @@ class Data:
     @staticmethod
     def _validate_data(data) -> None:
         if isinstance(data, datasets.DatasetDict):
-            raise ValueError(
-                "Please pass a single dataset, not a DatasetDict. "
-                "Try initializing with data['train'] instead."
-            )
-
-        assert isinstance(data, Dataset)
+            raise DatasetDictError()
+        if not isinstance(data, (Dataset, pd.DataFrame, dict, list, str)):
+            raise DataFormatError(data)
 
     @staticmethod
     def _validate_data_and_labels(data, labels) -> None:
@@ -149,6 +166,43 @@ class Data:
 
         if isinstance(labels, str):
             pass
+
+    @staticmethod
+    def _load_dataset_from_dict(data_dict: Dict[str, Any]) -> Dataset:
+        try:
+            return Dataset.from_dict(data_dict)
+        except Exception as error:
+            raise DatasetLoadError(dict) from error
+
+    @staticmethod
+    def _load_dataset_from_list(data_list: List[Dict[str, Any]]) -> Dataset:
+        try:
+            return Dataset.from_list(data_list)
+        except Exception as error:
+            raise DatasetLoadError(list) from error
+
+    @staticmethod
+    def _load_dataset_from_string(data_string: str) -> Dataset:
+        if not os.path.exists(data_string):
+            try:
+                dataset = datasets.load_dataset(data_string)
+                return cast(Dataset, dataset)
+            except Exception as error:
+                raise DatasetLoadError(str) from error
+
+        factory: Dict[str, Callable[[str], Any]] = {
+            ".txt": Dataset.from_text,
+            ".csv": Dataset.from_csv,
+            ".json": Dataset.from_json,
+        }
+
+        extension = os.path.splitext(data_string)[1]
+        if extension not in factory:
+            raise DatasetLoadError(type(data_string))
+
+        dataset = factory[extension](data_string)
+        dataset_cast = cast(Dataset, dataset)
+        return dataset_cast
 
 
 def _extract_labels(data: Dataset, label_name: Union[str, List[str]]) -> Tuple[np.ndarray, Mapping]:
