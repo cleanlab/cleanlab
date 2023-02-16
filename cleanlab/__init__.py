4b52f89f39e19d3f0c7b83c677574931a6bd5303
Elías Snorrason <eliassno@gmail.com>
2023-02-16 00:25:07 +0000

    fix(datalab): 🥅 handle ImportError for missing optional dependencies
    Use placeholder object that raises an error if called or attempts to access attributes.

--- cleanlab/__init__.py
+++ cleanlab/__init__.py
@@ -9,5 +9,46 @@ from . import multiannotator
 from . import outlier
 from . import token_classification
 from . import multilabel_classification
-from .experimental.datalab.datalab import Datalab
-from .experimental.datalab.issue_manager import IssueManager
+
+
+class DatalabUnavailable:
+    def __init__(self, message):
+        self.message = message
+
+    def __getattr__(self, name):
+        message = self.message + f" (raised when trying to access {name})"
+        raise ImportError(message)
+
+    def __call__(self, *args, **kwargs):
+        message = (
+            self.message + f" (raised when trying to call with args: {args}, kwargs: {kwargs})"
+        )
+        raise ImportError(message)
+
+
+def _datalab_import_factory():
+    try:
+        from .experimental.datalab.datalab import Datalab
+
+        return Datalab
+    except ImportError:
+        return DatalabUnavailable(
+            "Datalab is not available due to missing dependencies. "
+            "To install Datalab, run `pip install cleanlab[datalab]`."
+        )
+
+
+def _issue_manager_import_factory():
+    try:
+        from .experimental.datalab.issue_manager import IssueManager
+
+        return IssueManager
+    except ImportError:
+        return DatalabUnavailable(
+            "IssueManager is not available due to missing dependencies for Datalab. "
+            "To install Datalab, run `pip install cleanlab[datalab]`."
+        )
+
+
+Datalab = _datalab_import_factory()
+IssueManager = _issue_manager_import_factory()
