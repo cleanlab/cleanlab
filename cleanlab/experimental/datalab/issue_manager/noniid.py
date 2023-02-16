08217cea30d9bf3e6f0de291607ff340155f11bd
Jesse Cummings <jecummin@mit.edu>
2023-02-16 08:30:31 -0500

    Add non-iid issue manager

---
+++ cleanlab/experimental/datalab/issue_manager/noniid.py
@@ -0,0 +1,269 @@
+from __future__ import annotations
+
+
+from typing import Any, Dict, List, Optional, TYPE_CHECKING, Union
+
+import scipy
+import pandas as pd
+import numpy as np
+from fast_histogram import histogram1d
+from sklearn.neighbors import NearestNeighbors
+from sklearn.utils.validation import check_is_fitted
+
+from cleanlab.experimental.datalab.issue_manager import IssueManager
+
+if TYPE_CHECKING:  # pragma: no cover
+    from cleanlab.experimental.datalab.datalab import Datalab
+
+
+#TODO typing and method signatures
+
+class NonIIDIssueManager(IssueManager):  # pragma: no cover
+    """Manages issues realted to non-iid data distributions."""
+
+    issue_name: str = "non_iid"
+
+    def __init__(
+        self,
+        datalab: Datalab,
+        metric: Optional[str] = "cosine",
+        threshold: Optional[float] = None,
+        k: Optional[int] = 10,
+        num_permutations: Optional[int] = 25,
+        **_,
+    ):
+
+        super().__init__(datalab)
+        self.metric = metric
+        self.threshold = threshold
+        self.k = k
+        self.num_permutations = num_permutations
+        self.knn = None
+        self.tests = {
+            'ks': self._ks_test,
+        }
+        # TODO
+
+    def find_issues(
+        self,
+            features: List[str],
+            **_,
+    ) -> None:
+
+        feature_array = self._extract_embeddings(features)
+
+        if self.knn is None:
+            self.knn = NearestNeighbors(n_neighbors=self.k, metric=self.metric)
+
+        try:
+            check_is_fitted(self.knn)
+        except:
+            self.knn.fit(feature_array)
+
+        self.neighbor_graph = self._get_neighbor_graph()
+
+        self.num_neighbors = self.k
+        self.num_non_neighbors = min(10 * self.num_neighbors, len(self.neighbor_graph) - self.num_neighbors - 1)
+        self.neighbor_index_distances = self._sample_neighbors(num_samples=self.num_neighbors)
+        self.non_neighbor_index_distances = self._sample_non_neighbors(num_samples=self.num_non_neighbors)
+        neighbor_histogram = self._build_histogram(self.neighbor_index_distances.flatten())
+        non_neighbor_histogram = self._build_histogram(self.non_neighbor_index_distances.flatten())
+
+        self.statistics = self._get_statistics(
+            neighbor_histogram,
+            non_neighbor_histogram
+        )
+
+        self.p_value = self._permutation_test(num_permutations=self.num_permutations)
+
+        # TODO what about scores?
+        scores = self._score_dataset()
+        self.issues = pd.DataFrame(
+            {
+                # f"is_{self.issue_name}_issue": scores < self.threshold,  # TODO this doesn't make sense
+                self.issue_score_key: scores,
+            },
+        )
+
+        self.summary = self.get_summary(score=self.p_value) # TODO is the p-value the right thing to include here?
+
+        self.info = self.collect_info()
+
+    def collect_info(self) -> dict:
+        issues_dict = {
+            "p-value": self.p_value,
+        }
+
+        params_dict = {
+            "metric": self.metric,
+            "k": self.k,
+            "threshold": self.threshold,
+        }
+
+        knn = self.knn
+        dists, nn_ids = [array[:, 0] for array in knn.kneighbors()]  # type: ignore[union-attr]
+        weighted_knn_graph = knn.kneighbors_graph(mode="distance").toarray()  # type: ignore[union-attr]
+
+        # TODO: Reverse the order of the calls to knn.kneighbors() and knn.kneighbors_graph()
+        #   to avoid computing the (distance, id) pairs twice.
+        knn_info_dict = {
+            "nearest_neighbour": nn_ids.tolist(),
+            "distance_to_nearest_neighbour": dists.tolist(),
+            # TODO Check scipy-dependency
+            "weighted_knn_graph": weighted_knn_graph.tolist(),
+        }
+
+
+        info_dict = {
+            **issues_dict,
+            **params_dict,
+            **knn_info_dict,
+        }
+        return info_dict
+
+        
+
+    def _permutation_test(self, num_permutations):
+        graph = self.neighbor_graph
+        tiled = np.tile(np.arange(len(graph)), (len(graph), 1))
+        index_distances = tiled - tiled.transpose()
+        neighbors = graph > 0
+        others = graph < 0
+
+        statistics = []
+        for i in range(num_permutations):
+            perm = np.random.permutation(len(graph))
+            distance = (perm - np.arange(len(graph))).reshape(len(graph), 1)
+            new_distances = np.abs(distance - index_distances - distance.transpose())
+        
+            neighbor_index_distances = self._sample_neighbors(distances=new_distances, num_samples=self.num_neighbors).flatten()
+            non_neighbor_index_distances = self._sample_non_neighbors(distances=new_distances, num_samples=self.num_non_neighbors).flatten()
+            neighbor_histogram = self._build_histogram(neighbor_index_distances)
+            non_neighbor_histogram = self._build_histogram(non_neighbor_index_distances)
+        
+            stats = self._get_statistics(
+                neighbor_histogram,
+                non_neighbor_histogram,
+            )
+            statistics.append(stats)
+            
+        ks_stats = np.array([stats['ks'] for stats in statistics])
+        ks_stats_kde = scipy.stats.gaussian_kde(ks_stats)
+        p_value = ks_stats_kde.integrate_box(self.statistics['ks'], 100)
+
+        return p_value
+
+    def _score_dataset(self):
+        graph = self.neighbor_graph
+        scores = {}    
+        
+        num_bins = len(graph) - 1
+        bin_range = (1, num_bins)
+    
+        neighbor_cdfs = self._compute_row_cdf(self.neighbor_index_distances, num_bins, bin_range)
+        non_neighbor_cdfs = self._compute_row_cdf(self.non_neighbor_index_distances, num_bins, bin_range)
+
+        stats = np.sum(np.abs(neighbor_cdfs - non_neighbor_cdfs), axis=1)
+
+        indices = np.arange(len(graph))
+        reverse = len(graph) - indices
+        normalizer = np.where(indices > reverse, indices, reverse)
+
+        scores = stats / normalizer
+        scores = np.tanh(-1 * scores) + 1
+        
+        scores = {idx: scores[idx] for idx in range(len(scores))}
+        return scores
+
+    def _compute_row_cdf(self, array, num_bins, bin_range):
+        histograms = np.apply_along_axis(lambda x: histogram1d(x, num_bins, bin_range), 1, array)
+        histograms = histograms / np.sum(histograms[0])
+
+        cdf = np.apply_along_axis(np.cumsum, 1, histograms)
+        return cdf
+        
+    def _extract_embeddings(self, columns: Union[str, List[str]], **kwargs) -> np.ndarray:
+        """Extracts embeddings for the given columns."""
+        
+        # TODO this method is copied by outlier, duplicate, and non-iid managers. Perhaps it should be placed such that they can share it withough copying code
+
+        if isinstance(columns, list):
+            raise NotImplementedError("TODO: Support list of columns.")
+
+        format_kwargs = kwargs.get("format_kwargs", {})
+
+        return self.datalab.data.with_format("numpy", **format_kwargs)[columns]
+
+    def _get_neighbor_graph(self):
+        """
+        Given a fitted knn object, returns an array in which A[i,j] = n if
+        item i and j are nth nearest neighbors. For n > k, A[i,j] = -1. Additionally, A[i,i] = 0
+        """
+        
+        distances, kneighbors = self.knn.kneighbors()
+        graph = self.knn.kneighbors_graph(n_neighbors=self.k).toarray()
+
+        kneighbor_graph = np.ones(graph.shape) * -1
+        for i, nbrs in enumerate(kneighbors):
+            kneighbor_graph[i,nbrs] = 1 + np.arange(len(nbrs))
+            kneighbor_graph[i,i] = 0
+        return kneighbor_graph
+
+    def _ks_test(
+            self,
+            neighbor_histogram,
+            non_neighbor_histogram,
+    ):
+        neighbor_cdf = np.array([np.sum(neighbor_histogram[:i]) for i in range(len(neighbor_histogram) + 1)])
+        non_neighbor_cdf = np.array([np.sum(non_neighbor_histogram[:i]) for i in range(len(non_neighbor_histogram) + 1)])
+    
+        statistic = np.max(np.abs(neighbor_cdf - non_neighbor_cdf))
+        return statistic
+
+    def _get_statistics(
+            self,
+            neighbor_index_distances,
+            non_neighbor_index_distances,
+    ):
+
+        statistics = {}
+        for key, test in self.tests.items():
+            statistic = test(
+                neighbor_index_distances,
+                non_neighbor_index_distances,
+            )
+            statistics[key] = statistic
+        return statistics
+
+    def _sample_distances(self, sample_neighbors, distances=None, num_samples=1):
+        graph = self.neighbor_graph
+        all_idx = np.arange(len(graph))
+        all_idx = np.tile(all_idx, (len(graph),1))
+        if sample_neighbors:
+            indices = all_idx[graph > 0].reshape(len(graph), -1)
+        else:
+            indices = all_idx[graph < 0].reshape(len(graph), -1)
+        generator = np.random.default_rng()
+        choices = generator.choice(indices, axis=1, size=num_samples, replace=False)
+        if distances is None:
+            sample_distances = np.abs(np.arange(len(graph)) - choices.transpose()).transpose()
+        else:
+            sample_distances = distances[np.arange(len(graph)), choices.transpose()].transpose()
+        return sample_distances
+
+    def _sample_neighbors(self, distances=None, num_samples=1):
+        return self._sample_distances(sample_neighbors=True, distances=distances, num_samples=num_samples)
+
+    def _sample_non_neighbors(self, distances=None, num_samples=1):
+        return self._sample_distances(sample_neighbors=False, distances=distances, num_samples=num_samples)
+
+    def _build_histogram(self, index_array):
+        num_bins = len(self.neighbor_graph) - 1
+        bin_range = (1, num_bins)
+        histogram = histogram1d(index_array, num_bins, bin_range)
+        histogram = histogram /  len(index_array)
+        return histogram
+
+    
+
+    
