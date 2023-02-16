380af955bb5071296a1ede23f943cd8ff5dbe8b5
Elías Snorrason <eliassno@gmail.com>
2023-02-16 15:43:59 +0000

    chore(datalab): 📄 add licence and bump year

--- cleanlab/experimental/datalab/issue_manager/duplicate.py
+++ cleanlab/experimental/datalab/issue_manager/duplicate.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2017-2023  Cleanlab Inc.
+# This file is part of cleanlab.
+#
+# cleanlab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cleanlab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, ClassVar, List, Optional, Tuple, cast
