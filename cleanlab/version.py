590eee1f3c13e4a1ded0c21d4b2f63c93daed13f
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2023-03-27 23:48:33 -0700

    bump git version past stable version

--- cleanlab/version.py
+++ cleanlab/version.py
@@ -15,7 +15,7 @@
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 
 
-__version__ = "2.3.1"
+__version__ = "2.3.2"
 
 # 2.3.2 - Not yet released, you are using bleeding-edge developer version. See its documentation at: https://docs.cleanlab.ai/master/
 
