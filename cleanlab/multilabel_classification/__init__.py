3894e52bb000bf6e65609680644789c9b63b9cc7
Aditya Thyagarajan <aditya1593@icloud.com>
2023-03-31 00:24:33 +0530

    more noqa

--- cleanlab/multilabel_classification/__init__.py
+++ cleanlab/multilabel_classification/__init__.py
@@ -1,2 +1,2 @@
 from cleanlab.internal.multilabel_scorer import MultilabelScorer, ClassLabelScorer, Aggregator # noqa
-from .label_quality_scores import  get_label_quality_scores
+from .label_quality_scores import  get_label_quality_scores # noqa
