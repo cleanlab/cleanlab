8b4363d5e68d49bca14cd03ddbd1798eb4d501bb
Aditya Thyagarajan <aditya1593@icloud.com>
2023-03-31 00:25:37 +0530

    lint

--- cleanlab/multilabel_classification/__init__.py
+++ cleanlab/multilabel_classification/__init__.py
@@ -1,2 +1,6 @@
-from cleanlab.internal.multilabel_scorer import MultilabelScorer, ClassLabelScorer, Aggregator # noqa
-from .label_quality_scores import  get_label_quality_scores # noqa
+from cleanlab.internal.multilabel_scorer import (
+    MultilabelScorer,
+    ClassLabelScorer,
+    Aggregator,
+)  # noqa
+from .label_quality_scores import get_label_quality_scores  # noqa
