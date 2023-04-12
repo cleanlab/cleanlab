b3c6ea1e8c4cf178d1fd0c937de749ec6da239bf
Aditya Thyagarajan <aditya1593@icloud.com>
2023-04-13 02:23:30 +0530

    improved docstring?

--- cleanlab/multilabel_classification/filter.py
+++ cleanlab/multilabel_classification/filter.py
@@ -194,9 +194,15 @@ def find_multilabel_issues_per_class(
 
         For more details, refer to :py:func:`cleanlab.multilabel_classification.filter.find_label_issues <cleanlab.multilabel_classification.filter.find_label_issues>`.
 
-        Otherwise if `return_indices_ranked_by` is not ``None``, then this method returns multiple objects.
-        The first of these objects is a list of length K whose k-th element is:
+        Otherwise if `return_indices_ranked_by` is not ``None``, then this method returns 3 objects: label_issues_list, labels_list, pred_probs_list
+        label_issues_list is a list of length K whose k-th element is:
         an ordered list of indices of examples where class k appears incorrectly annotated, sorted by the likelihood that class k is correctly annotated.
+        labels_list is a list of length K whose k-th element is:
+        a list of binary one-hot encoded labels where each element of the list indicates whether the example belongs to class k or not.
+        pred_probs_list is a list of length K whose k-th element is:
+        an array of predicted probabilities in a one-vs-rest setting of shape (N, 2), where first element has probability p
+        of belonging to that class, and the other (1-p)
+
     """
     import cleanlab.filter
     from cleanlab.internal.multilabel_utils import get_onehot_num_classes, stack_complement
