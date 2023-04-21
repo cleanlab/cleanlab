82e1a323e39a5501c70c265a01238247062c7eb6
Elías Snorrason <eliassno@gmail.com>
2023-04-21 15:37:06 +0000

    fix(datalab): address comments in datalab.display module

--- cleanlab/datalab/display.py
+++ cleanlab/datalab/display.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 """
-Module for class that handles the string representation of Datalab objects.
+Module that handles the string representation of Datalab objects.
 """
 
 from typing import TYPE_CHECKING
@@ -30,20 +30,32 @@ class _Displayer:
     def __repr__(self) -> str:
         """What is displayed in console if user executes: >>> datalab"""
         checks_run = not self.datalab.issues.empty
-        display_str = f"checks_run={checks_run},"
+        display_str = f"checks_run={checks_run}"
         num_examples = self.datalab.get_info("statistics")["num_examples"]
         if num_examples is not None:
-            display_str += f"num_examples={num_examples},"
+            display_str += f", num_examples={num_examples}"
         num_classes = self.datalab.get_info("statistics")["num_classes"]
         if num_classes is not None:
-            display_str += f"num_classes={num_classes},"
-        if display_str[-1] == ",":  # delete trailing comma
-            display_str = display_str[:-1]
-
-        # Useful info could be: num_examples, task, issues_identified
-        # (numeric or None if issue-finding not run yet).
+            display_str += f", num_classes={num_classes}"
+        if checks_run:
+            issues_identified = self.datalab.issue_summary["num_issues"].sum()
+            display_str += f", issues_identified={issues_identified}"
         return f"Datalab({display_str})"
 
     def __str__(self) -> str:
         """What is displayed if user executes: print(datalab)"""
-        return "Datalab"  # TODO
+        checks_run = not self.datalab.issues.empty
+        num_examples = self.datalab.get_info("statistics").get("num_examples")
+        num_classes = self.datalab.get_info("statistics").get("num_classes")
+
+        issues_identified = (
+            self.datalab.issue_summary["num_issues"].sum() if checks_run else "Not checked"
+        )
+        info_list = [
+            f"Checks run: {'Yes' if checks_run else 'No'}",
+            f"Number of examples: {num_examples if num_examples is not None else 'Unknown'}",
+            f"Number of classes: {num_classes if num_classes is not None else 'Unknown'}",
+            f"Issues identified: {issues_identified}",
+        ]
+
+        return "Datalab:\n" + "\n".join(info_list)
