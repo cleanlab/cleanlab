c5af24340d6cdccc5caf1d76b11219016482d900
OrdoAbChao <marek.konrad.glowacki@gmail.com>
2023-08-07 13:56:53 +0200

    Update issue_manager_factory.py

--- cleanlab/datalab/internal/issue_manager_factory.py
+++ cleanlab/datalab/internal/issue_manager_factory.py
@@ -94,8 +94,8 @@ class _IssueManagerFactory:
             raise ValueError(
                 "issue_type must be a string, not a list. Try using from_list instead."
             )
-            
-        #TODO: refactor it
+
+        # TODO: refactor it
         if task is None:
             if issue_type not in REGISTRY:
                 raise ValueError(f"Invalid issue type: {issue_type}")
@@ -107,7 +107,7 @@ class _IssueManagerFactory:
 
             if issue_type not in TASK_SPECIFIC_REGISTRY[task] and issue_type not in REGISTRY:
                 raise ValueError(f"Invalid issue type: {issue_type} for task {task}")
-                
+
             if issue_type in TASK_SPECIFIC_REGISTRY[task]:
                 REGISTRY[issue_type] = TASK_SPECIFIC_REGISTRY[task][issue_type]
 
