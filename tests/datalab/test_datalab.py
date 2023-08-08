45d2a59cc2926985f644a5787f9f5e922f826766
OrdoAbChao <marek.konrad.glowacki@gmail.com>
2023-08-08 07:05:35 +0200

    Update test_datalab.py

--- tests/datalab/test_datalab.py
+++ tests/datalab/test_datalab.py
@@ -684,7 +684,9 @@ def test_report_for_outlier_issues_via_pred_probs(find_issues_kwargs):
     find_issues_kwargs["issue_types"] = {"outlier": {"k": 1}}
     lab.find_issues(**find_issues_kwargs)
 
-    reporter = Reporter(lab.data_issues, task=None, verbosity=0, include_description=False)
+    reporter = Reporter(
+        lab.data_issues, task="classification", verbosity=0, include_description=False
+    )
     report = reporter.get_report(num_examples=3)
     assert report, "Report should not be empty"
 
