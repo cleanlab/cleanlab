6b2f27c0673925f95ff21153c0febe910fe6e995
Chenhe Gu <chenhegu0109@gmail.com>
2023-10-07 15:33:19 +0800

    update unit test

--- tests/datalab/issue_manager/test_data_valuation.py
+++ tests/datalab/issue_manager/test_data_valuation.py
@@ -24,7 +24,7 @@ class TestDataValuationIssueManager:
         embeddings_array[4, :] = -1
         return {"embedding": embeddings_array}
 
-    def test_find_issues(self, issue_manager, embeddings):
+    def test_find_issues_with_input(self, issue_manager, embeddings):
         outlier_issue_manager = OutlierIssueManager(datalab=issue_manager.datalab, k=3)
         outlier_issue_manager.find_issues(features=embeddings["embedding"])
         knn_graph = outlier_issue_manager._process_knn_graph_from_features({})
@@ -45,3 +45,29 @@ class TestDataValuationIssueManager:
         assert all(
             [key in info_keys for key in expected_keys]
         ), f"Info should have the right keys, but is missing {set(expected_keys) - set(info_keys)}"
+
+    def test_find_issues_with_stats(self, issue_manager, embeddings):
+        issue_manager.datalab.find_issues(
+            features=embeddings["embedding"], issue_types={"outlier": {"k": 3}}
+        )
+        issue_manager.find_issues()
+        issues, summary, info = issue_manager.issues, issue_manager.summary, issue_manager.info
+        assert isinstance(issues, pd.DataFrame), "Issues should be a dataframe"
+
+        assert isinstance(summary, pd.DataFrame), "Summary should be a dataframe"
+        assert summary["issue_type"].values[0] == "data_valuation"
+
+        assert isinstance(info, dict), "Info should be a dict"
+        assert isinstance(issues, pd.DataFrame), "Issues should be a dataframe"
+        info_keys = info.keys()
+        expected_keys = [
+            "num_low_valuation_issues",
+            "average_data_valuation",
+        ]
+        assert all(
+            [key in info_keys for key in expected_keys]
+        ), f"Info should have the right keys, but is missing {set(expected_keys) - set(info_keys)}"
+
+    def test_find_issue_no_knn_graph(self, issue_manager):
+        with pytest.raises(AssertionError):
+            issue_manager.find_issues()
