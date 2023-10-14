afa34e8b7414276ea7d8f9f988604f7735b1da77
abhijitpal1247 <abhijit@samasra.net>
2023-10-14 19:20:23 +0530

    adding some more metrics to track and test them

--- cleanlab/datalab/internal/issue_manager/null.py
+++ cleanlab/datalab/internal/issue_manager/null.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, Optional
+from collections import Counter
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, Optional, List, Tuple
 
 import numpy as np
 import pandas as pd
+from numpy import ndarray
 
 from cleanlab.datalab.internal.issue_manager import IssueManager
 
@@ -27,20 +29,17 @@ class NullIssueManager(IssueManager):
     issue_name: ClassVar[str] = "null"
     verbosity_levels = {
         0: ["average_null_score"],
-        1: [],
+        1: [
+            "most_common_issue",
+        ],
         2: [],
     }
 
     def __init__(self, datalab: Datalab):
         super().__init__(datalab)
 
-    def find_issues(
-        self,
-        features: Optional[npt.NDArray] = None,
-        **kwargs,
-    ) -> None:
-        if features is None:
-            raise ValueError("features must be provided to check for null values.")
+    @staticmethod
+    def _calculate_null_issues(features: npt.NDArray) -> tuple[ndarray, ndarray, Any]:
         rows = features.shape[0]
         cols = features.shape[1]
         scores = np.zeros(rows).astype(np.float32)
@@ -49,9 +48,20 @@ class NullIssueManager(IssueManager):
         if null_tracker.any():
             for row in range(rows):
                 if null_tracker[row].any():
-                    is_null_issue[row] = True
-                    null_row_count = np.count_nonzero(null_tracker[row])
+                    null_row_count = np.count_nonzero(~null_tracker[row])
                     scores[row] = null_row_count / cols
+                    if scores[row] == 0.00:
+                        is_null_issue[row] = True
+        return is_null_issue, scores, null_tracker
+
+    def find_issues(
+        self,
+        features: Optional[npt.NDArray] = None,
+        **kwargs,
+    ) -> None:
+        if features is None:
+            raise ValueError("features must be provided to check for null values.")
+        is_null_issue, scores, null_tracker = self._calculate_null_issues(features=features)
 
         self.issues = pd.DataFrame(
             {
@@ -61,9 +71,78 @@ class NullIssueManager(IssueManager):
         )
 
         self.summary = self.make_summary(score=scores.mean())
-        self.info = self.collect_info()
+        self.info = self.collect_info(null_tracker)
 
-    def collect_info(self) -> dict:
-        issues_dict = {"average_null_score": self.issues[self.issue_score_key].mean()}
+    @staticmethod
+    def most_common_issue(null_tracker: np.ndarray) -> Dict[str, Dict[str, float]]:
+        """
+        Identify and return the most common null value pattern across all rows
+        and count the number of rows with this pattern.
+
+        Parameters
+        ------------
+        null_tracker : np.ndarray
+            A boolean array of the same shape as features, where True indicates null/missing entries.
+
+        Returns
+        --------
+        Dict[str, Any]
+            A dictionary containing the most common issue pattern and the count of rows with this pattern.
+        """
+        # Convert the boolean null_tracker matrix into a list of strings.
+        most_frequent_pattern = "no_null"
+        rows_affected = []
+        occurrence_of_most_frequent_pattern = 0
+        if null_tracker.any():
+            null_patterns_as_strings = [
+                "".join(map(str, row.astype(int).tolist())) for row in null_tracker if row.any()
+            ]
+
+            # Use Counter to efficiently count occurrences and find the most common pattern.
+            pattern_counter = Counter(null_patterns_as_strings)
+            (
+                most_frequent_pattern,
+                occurrence_of_most_frequent_pattern,
+            ) = pattern_counter.most_common(1)[0]
+            rows_affected = []
+            for idx, row in enumerate(null_patterns_as_strings):
+                if row == most_frequent_pattern:
+                    rows_affected.append(idx)
+        return {
+            "most_common_issue": {
+                "pattern": most_frequent_pattern,
+                "rows_affected": rows_affected,
+                "count": occurrence_of_most_frequent_pattern,
+            }
+        }
+
+    @staticmethod
+    def column_impact(null_tracker: np.ndarray) -> Dict[str, List[float]]:
+        """
+        Calculate and return the impact of null values per column, represented as the proportion
+        of rows having null values in each column.
+
+        Parameters
+        ----------
+        null_tracker : np.ndarray
+            A boolean array of the same shape as features, where True indicates null/missing entries.
+
+        Returns
+        -------
+        Dict[str, List[float]]
+            A dictionary containing the impact per column, with values being a list
+            where each element is the percentage of rows having null values in the corresponding column.
+        """
+        # Calculate proportion of nulls in each column
+        proportion_of_nulls_per_column = null_tracker.mean(axis=0)
+
+        # Return result as a dictionary containing a list of proportions
+        return {"column_impact": proportion_of_nulls_per_column.tolist()}
+
+    def collect_info(self, null_tracker: np.ndarray) -> dict:
+        most_common_issue = self.most_common_issue(null_tracker=null_tracker)
+        column_impact = self.column_impact(null_tracker=null_tracker)
+        average_null_score = {"average_null_score": self.issues[self.issue_score_key].mean()}
+        issues_dict = {**average_null_score, **most_common_issue, **column_impact}
         info_dict: Dict[str, Any] = {**issues_dict}
         return info_dict
--- tests/datalab/issue_manager/test_null.py
+++ tests/datalab/issue_manager/test_null.py
@@ -10,15 +10,18 @@ SEED = 42
 
 class TestNullIssueManager:
     @pytest.fixture
-    def embeddings(self, request):
-        no_null = request.param
+    def embeddings(self):
         np.random.seed(SEED)
         embeddings_array = np.random.random((4, 3))
-        if not no_null:
-            embeddings_array[0][0] = np.NaN
-            embeddings_array[2][1] = np.NaN
-            embeddings_array[2][2] = np.NaN
-        return embeddings_array, no_null
+        return embeddings_array
+
+    @pytest.fixture
+    def embeddings_with_null(self):
+        np.random.seed(SEED)
+        embeddings_array = np.random.random((4, 3))
+        embeddings_array[0][0] = np.NaN
+        embeddings_array[1] = np.NaN
+        return embeddings_array
 
     @pytest.fixture
     def issue_manager(self, lab):
@@ -27,47 +30,50 @@ class TestNullIssueManager:
     def test_init(self, lab, issue_manager):
         assert issue_manager.datalab == lab
 
-    @pytest.mark.parametrize("embeddings", [True, False], indirect=["embeddings"])
     def test_find_issues(self, issue_manager, embeddings):
         np.random.seed(SEED)
-        embeddings, no_null_flag = embeddings
         issue_manager.find_issues(features=embeddings)
         issues_sort, summary_sort, info_sort = (
             issue_manager.issues,
             issue_manager.summary,
             issue_manager.info,
         )
-        if no_null_flag:
-            expected_sorted_issue_mask = np.array([False, False, False, False])
-            assert np.all(
-                issues_sort["is_null_issue"] == expected_sorted_issue_mask
-            ), "Issue mask should be correct"
-            assert summary_sort["issue_type"][0] == "null"
-            assert summary_sort["score"][0] == pytest.approx(expected=0.0, abs=1e-7)
-            assert (
-                info_sort.get("average_null_score", None) is not None
-            ), "Should have average null score"
-            assert summary_sort["score"][0] == pytest.approx(
-                expected=info_sort["average_null_score"], abs=1e-7
-            )
-        else:
-            expected_sorted_issue_mask = np.array([True, False, True, False])
-            assert np.all(
-                issues_sort["is_null_issue"] == expected_sorted_issue_mask
-            ), "Issue mask should be correct"
-            assert summary_sort["issue_type"][0] == "null"
-            assert summary_sort["score"][0] == pytest.approx(expected=0.25, abs=1e-7)
-            assert (
-                info_sort.get("average_null_score", None) is not None
-            ), "Should have average null score"
-            assert summary_sort["score"][0] == pytest.approx(
-                expected=info_sort["average_null_score"], abs=1e-7
-            )
+        expected_sorted_issue_mask = np.array([False, False, False, False])
+        assert np.all(
+            issues_sort["is_null_issue"] == expected_sorted_issue_mask
+        ), "Issue mask should be correct"
+        assert summary_sort["issue_type"][0] == "null"
+        assert summary_sort["score"][0] == pytest.approx(expected=0.0, abs=1e-7)
+        assert (
+            info_sort.get("average_null_score", None) is not None
+        ), "Should have average null score"
+        assert summary_sort["score"][0] == pytest.approx(
+            expected=info_sort["average_null_score"], abs=1e-7
+        )
 
-    @pytest.mark.parametrize("embeddings", [True, False], indirect=["embeddings"])
+    def test_find_issues_with_null(self, issue_manager, embeddings_with_null):
+        np.random.seed(SEED)
+        issue_manager.find_issues(features=embeddings_with_null)
+        issues_sort, summary_sort, info_sort = (
+            issue_manager.issues,
+            issue_manager.summary,
+            issue_manager.info,
+        )
+        expected_sorted_issue_mask = np.array([False, True, False, False])
+        assert np.all(
+            issues_sort["is_null_issue"] == expected_sorted_issue_mask
+        ), "Issue mask should be correct"
+        assert summary_sort["issue_type"][0] == "null"
+        assert summary_sort["score"][0] == pytest.approx(expected=0.16666667, abs=1e-7)
+        assert (
+            info_sort.get("average_null_score", None) is not None
+        ), "Should have average null score"
+        assert summary_sort["score"][0] == pytest.approx(
+            expected=info_sort["average_null_score"], abs=1e-7
+        )
+
     def test_report(self, issue_manager, embeddings):
         np.random.seed(SEED)
-        embeddings, no_null_flag = embeddings
         issue_manager.find_issues(features=embeddings)
         report = issue_manager.report(
             issues=issue_manager.issues,
@@ -89,13 +95,45 @@ class TestNullIssueManager:
         )
         assert "Additional Information: " in report
 
-    @pytest.mark.parametrize("embeddings", [True, False], indirect=["embeddings"])
+    def test_report_with_null(self, issue_manager, embeddings_with_null):
+        np.random.seed(SEED)
+        issue_manager.find_issues(features=embeddings_with_null)
+        report = issue_manager.report(
+            issues=issue_manager.issues,
+            summary=issue_manager.summary,
+            info=issue_manager.info,
+        )
+
+        assert isinstance(report, str)
+        assert (
+            "----------------------- null issues ------------------------\n\n"
+            "Number of examples with this issue:"
+        ) in report
+
+        report = issue_manager.report(
+            issues=issue_manager.issues,
+            summary=issue_manager.summary,
+            info=issue_manager.info,
+            verbosity=3,
+        )
+        assert "Additional Information: " in report
+
     def test_collect_info(self, issue_manager, embeddings):
         """Test some values in the info dict."""
-        embeddings, no_null_flag = embeddings
         issue_manager.find_issues(features=embeddings)
         info = issue_manager.info
-        if no_null_flag:
-            assert info["average_null_score"] == 0.0
-        else:
-            assert info["average_null_score"] == 0.25
+        assert info["average_null_score"] == 0.0
+        assert info["most_common_issue"]["pattern"] == "no_null"
+        assert info["most_common_issue"]["count"] == 0
+        assert info["most_common_issue"]["rows_affected"] == []
+        assert info["column_impact"] == [0, 0, 0]
+
+    def test_collect_info_with_nulls(self, issue_manager, embeddings_with_null):
+        """Test some values in the info dict."""
+        issue_manager.find_issues(features=embeddings_with_null)
+        info = issue_manager.info
+        assert info["average_null_score"] == pytest.approx(expected=0.16666667, abs=1e-7)
+        assert info["most_common_issue"]["pattern"] == "100"
+        assert info["most_common_issue"]["count"] == 1
+        assert info["most_common_issue"]["rows_affected"] == [0]
+        assert info["column_impact"] == [0.5, 0.25, 0.25]
