e5ddd7a7d3da31224119534fa4b61e47b0abab13
abhijitpal1247 <abhijit@samasra.net>
2023-10-16 18:26:02 +0530

    removing unnecessary bool

--- cleanlab/datalab/internal/issue_manager/noniid.py
+++ cleanlab/datalab/internal/issue_manager/noniid.py
@@ -197,7 +197,7 @@ class NonIIDIssueManager(IssueManager):
     ) -> None:
         knn_graph = self._process_knn_graph_from_inputs(kwargs)
         old_knn_metric = self.datalab.get_info("statistics").get("knn_metric")
-        metric_changes = True if (self.metric and self.metric != old_knn_metric) else False
+        metric_changes = self.metric and self.metric != old_knn_metric
 
         # This block of code has been extracted to a helper method for readability and making the code more modular.
         knn, features_used = self._select_features_and_setup_knn(
