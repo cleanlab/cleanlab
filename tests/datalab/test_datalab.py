ccb4a90e38f6734aa4a12de0f6fd297688dc2ba0
ABHIJIT PAL <abhijit1247@gmail.com>
2023-10-30 08:25:53 +0530

    Extend non-iid issue check to run if only pred_probs are provided (#857)
    Co-authored-by: Elías Snorrason <eliassno@gmail.com>

--- tests/datalab/test_datalab.py
+++ tests/datalab/test_datalab.py
@@ -776,10 +776,30 @@ class TestDatalabFindNonIIDIssues:
         assert summary["score"].values[0] > 0.05
         assert lab.get_issues()["is_non_iid_issue"].sum() == 0
 
+    def test_find_non_iid_issues_using_pred_probs(self, random_embeddings):
+        data = {"labels": [0, 1, 0]}
+        lab = Datalab(data=data, label_name="labels")
+        pred_probs = random_embeddings / random_embeddings.sum(axis=1, keepdims=True)
+        lab.find_issues(pred_probs=pred_probs, issue_types={"non_iid": {}})
+        summary = lab.get_issue_summary()
+        assert ["non_iid"] == summary["issue_type"].values
+        assert summary["score"].values[0] > 0.05
+        assert lab.get_issues()["is_non_iid_issue"].sum() == 0
+
     def test_find_non_iid_issues_sorted(self, sorted_embeddings):
         data = {"labels": [0, 1, 0]}
         lab = Datalab(data=data, label_name="labels")
         lab.find_issues(features=sorted_embeddings, issue_types={"non_iid": {}})
+        summary = lab.get_issue_summary()
+        assert ["non_iid"] == summary["issue_type"].values
+        assert summary["score"].values[0] == 0
+        assert lab.get_issues()["is_non_iid_issue"].sum() == 1
+
+    def test_find_non_iid_issues_sorted_using_pred_probs(self, sorted_embeddings):
+        data = {"labels": [0, 1, 0]}
+        lab = Datalab(data=data, label_name="labels")
+        pred_probs = sorted_embeddings / sorted_embeddings.sum(axis=1, keepdims=True)
+        lab.find_issues(pred_probs=pred_probs, issue_types={"non_iid": {}})
         summary = lab.get_issue_summary()
         assert ["non_iid"] == summary["issue_type"].values
         assert summary["score"].values[0] == 0
@@ -794,6 +814,21 @@ class TestDatalabFindNonIIDIssues:
         lab.find_issues(features=sorted_embeddings, issue_types={"non_iid": {}})
         summary = lab.get_issue_summary()
         assert len(summary) == 3
+        assert "non_iid" in summary["issue_type"].values
+        non_iid_summary = lab.get_issue_summary("non_iid")
+        assert non_iid_summary["score"].values[0] == 0
+        assert non_iid_summary["num_issues"].values[0] == 1
+
+    def test_incremental_search_using_pred_probs(self, sorted_embeddings):
+        data = {"labels": [0, 1, 0]}
+        lab = Datalab(data=data, label_name="labels")
+        pred_probs = sorted_embeddings / sorted_embeddings.sum(axis=1, keepdims=True)
+        lab.find_issues(pred_probs=pred_probs, issue_types={"non_iid": {}})
+        summary = lab.get_issue_summary()
+        assert len(summary) == 1
+        lab.find_issues(pred_probs=pred_probs, issue_types={"non_iid": {}})
+        summary = lab.get_issue_summary()
+        assert len(summary) == 1
         assert "non_iid" in summary["issue_type"].values
         non_iid_summary = lab.get_issue_summary("non_iid")
         assert non_iid_summary["score"].values[0] == 0
@@ -1042,7 +1077,7 @@ class TestDatalabWithoutLabels:
     def test_find_issues(self, lab, features, pred_probs):
         lab = Datalab(data={"X": features})
         lab.find_issues(pred_probs=pred_probs)
-        assert lab.issues.empty
+        assert set(lab.issues.columns) == {"is_non_iid_issue", "non_iid_score"}
 
         lab = Datalab(data={"X": features})
         lab.find_issues(features=features)
