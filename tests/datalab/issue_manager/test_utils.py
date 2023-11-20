1a10a4b443c7ecc8c901aa7a19390577505c9c02
Pratham_Savaliya <pratham21btai35@karnavatiuniversity.edu.in>
2023-11-20 17:24:05 +0530

    update test_utils.py

--- tests/datalab/issue_manager/test_utils.py
+++ tests/datalab/issue_manager/test_utils.py
@@ -1,22 +1,61 @@
 import numpy as np
 import pytest
 
-# from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix
 
 from cleanlab.datalab.internal.issue_manager.utils import ConstructedKNNGraph
-from cleanlab.datalab.datalab import Datalab
 
 
 class TestConstructedKNNGraph:
     @pytest.fixture
-    def datalab_instance(self):
-        y = np.random.randint(0, 2, size=30)
-        lab = Datalab(data={"y": y}, label_name="y")
-        return lab
+    def sparse_matrix(self):
+        X = np.random.RandomState(0).rand(5, 5)
+        return csr_matrix(X)
 
-    def test_process_knn_graph_from_inputs_with_knn_graph(self, datalab_instance):
-        constructed_knn_graph_instance = ConstructedKNNGraph(
-            datalab_instance
-        ).process_knn_graph_from_inputs()
-        ## Todo
-        # assert (result_knn_graph != None)
+    @pytest.fixture
+    def constructed_knn_graph_instance(self, lab):
+        return ConstructedKNNGraph(lab)
+
+    def test_process_knn_graph_from_inputs_valid_graph(
+        self, constructed_knn_graph_instance, sparse_matrix
+    ):
+
+        # Check when knn_graph is present in "statistics"
+        lab = constructed_knn_graph_instance.datalab
+        lab.info["statistics"]["weighted_knn_graph"] = sparse_matrix  # Set key in statistics
+        knn_graph = constructed_knn_graph_instance.process_knn_graph_from_inputs(kwargs={})
+        assert isinstance(knn_graph, csr_matrix)  # Assert type
+        assert knn_graph is sparse_matrix
+
+        # Check when knn_graph is present in kwargs
+        kwargs = {"knn_graph": sparse_matrix}
+        knn_graph = constructed_knn_graph_instance.process_knn_graph_from_inputs(kwargs)
+        assert isinstance(knn_graph, csr_matrix)  # Assert type
+        assert (
+            knn_graph is sparse_matrix
+        )  # Assert that passed sparse matrix is same as returned knn graph
+
+        # Check when knn_graph is passed with k
+        kwargs = {"knn_graph": sparse_matrix, "k": 5}
+        knn_graph = constructed_knn_graph_instance.process_knn_graph_from_inputs(kwargs)
+        assert isinstance(knn_graph, csr_matrix)  # Assert type
+        assert (
+            knn_graph is sparse_matrix
+        )  # Assert that passed sparse matrix is same as returned knn graph
+
+    # Any other cases where valid knn_graph is returned
+
+    def test_process_knn_graph_from_inputs_return_None(
+        self, constructed_knn_graph_instance, sparse_matrix
+    ):
+
+        # First check for no knn graph (not present in kwargs or "weighted_knn_graph")
+        kwargs = {}
+        knn_graph = constructed_knn_graph_instance.process_knn_graph_from_inputs(kwargs)
+        assert knn_graph is None
+        # Pass knn_graph with larger k
+        kwargs = {"knn_graph": sparse_matrix, "k": 10}
+        knn_graph = constructed_knn_graph_instance.process_knn_graph_from_inputs(kwargs)
+        assert knn_graph is None
+
+        # Any other cases where returned knn_graph is None
