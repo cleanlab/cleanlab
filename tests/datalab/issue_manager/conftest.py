95f6868619ffcc40b042ae3c850543116c712100
Elías Snorrason <eliassno@gmail.com>
2023-11-27 19:51:54 +0000

    add docstring to composite hypothesis strategy for generating a knn_graph type

--- tests/datalab/issue_manager/conftest.py
+++ tests/datalab/issue_manager/conftest.py
@@ -5,6 +5,37 @@ from scipy.sparse import csr_matrix
 
 @st.composite
 def knn_graph_strategy(draw, num_samples, k_neighbors):
+    """
+    Generate a K-nearest neighbors (KNN) graph based on the given parameters.
+
+    Parameters
+    ----------
+    draw: A function used to draw values from search strategies.
+    
+    num_samples (int or SearchStrategy): The number of samples in the graph.
+        If a SearchStrategy is provided, a value will be drawn from it.
+    
+    k_neighbors (int or SearchStrategy): The number of nearest neighbors to consider for each sample.
+        If a SearchStrategy is provided, a value will be drawn from it.
+
+    Returns
+    -------
+    knn_graph : csr_matrix
+        The KNN graph represented as a sparse matrix.
+
+    Notes
+    -----
+    - The KNN graph is generated based on a symmetric distance matrix.
+    - The distance matrix is computed using randomly generated upper triangle values.
+    - The diagonal of the distance matrix is set to infinity to avoid selecting a point as its own neighbor.
+    - The K-nearest neighbors are computed based on the distance matrix.
+    - The resulting KNN graph is returned as a sparse matrix in csr format.
+    - The number of samples must be greater than the number of neighbors.
+    - The KNN graph is not guaranteed to be connected (i.e. there may be isolated subgraphs).
+    - The KNN graph is a directed graph (i.e. the edges are not symmetric).
+    - The neighbors are sorted by distance in the CSR-formatted sparse matrix,
+        so the first neighbor is the closest neighbor.
+    """
     # If the argument is a strategy, draw a value from it.
     if isinstance(num_samples, st.SearchStrategy):
         num_samples = draw(num_samples)
