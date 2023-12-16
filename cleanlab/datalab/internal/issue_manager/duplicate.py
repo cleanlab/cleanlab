1a22ed687a9d4a8f52d769e110840222cb45e210
ryansingman <ryanjs@vt.edu>
2023-12-15 18:12:00 -0700

    black fmt

--- cleanlab/datalab/internal/issue_manager/duplicate.py
+++ cleanlab/datalab/internal/issue_manager/duplicate.py
@@ -128,7 +128,7 @@ class NearDuplicateIssueManager(IssueManager):
         N = knn_graph.shape[0]
         distances = knn_graph.data.reshape(N, -1)
         # Create a mask for the threshold
-        median = max(np.median(distances[:, 0]), np.finfo(np.float_).eps) # avoid threshold = 0
+        median = max(np.median(distances[:, 0]), np.finfo(np.float_).eps)  # avoid threshold = 0
         mask = distances < threshold * median
 
         # Update the indptr to reflect the new number of neighbors
