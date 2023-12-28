b5744c8caa6a73ae69230b06ffd7967fc516ec66
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2023-12-29 00:02:59 +1300

    more specific unit test
    Co-authored-by: Ganesh Tata <tataganesh95@gmail.com>

--- tests/datalab/issue_manager/test_imbalance.py
+++ tests/datalab/issue_manager/test_imbalance.py
@@ -51,8 +51,10 @@ class TestClassImbalanceIssueManager:
         assert np.all(
             issues["is_class_imbalance_issue"] == np.full(N, False)
         ), "Issue mask should be correct"
-        assert np.min(issues["class_imbalance_score"]) == 0.47
-        assert np.max(issues["class_imbalance_score"]) == 1
+        scores = issues["class_imbalance_score"]
+        expected_scores = np.ones_like(scores)
+        expected_scores[labels == 1] = 0.47 # Rare class proportion
+        np.testing.assert_allclose(scores, expected_scores, err_msg="Scores should be correct")
         assert summary["issue_type"][0] == "class_imbalance"
         assert summary["score"][0] == 0.47
 
