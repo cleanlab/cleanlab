e9c2bfa7e4c4a39743ca8558f3675e867a83d15e
Aditya Thyagarajan <aditya1593@icloud.com>
2024-01-04 21:17:40 +0530

    add support for binary classifiers edge case

--- cleanlab/filter.py
+++ cleanlab/filter.py
@@ -446,7 +446,11 @@ def find_label_issues(
     if filter_by not in ["low_self_confidence", "low_normalized_margin"]:
         # Remove label issues if given label == model prediction if issues haven't been removed yet
         pred = pred_probs.argmax(axis=1)
-        label_issues_mask[pred == labels] = False
+        mask = pred == labels
+        if K == 2:
+            # Set mask to False wherever pred_probs == 0.5 where num_classes == 2
+            mask = mask & (pred_probs[:, 0] != 0.5)
+        label_issues_mask[mask] = False
 
     if verbose:
         print("Number of label issues found: {}".format(sum(label_issues_mask)))
