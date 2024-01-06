f5d80b34402def20bf5f22f7300f080cf7ab3828
Aditya Thyagarajan <aditya1593@icloud.com>
2024-01-06 15:35:13 +0530

    linting

--- cleanlab/count.py
+++ cleanlab/count.py
@@ -151,7 +151,11 @@ def num_label_issues(
         label_issues_mask[cl_error_indices] = True
 
         # Remove label issues if given label == model prediction
-        mask = _reduce_issues(pred_probs=pred_probs, labels=labels)
+        mask = _reduce_issues(
+            pred_probs=pred_probs,
+            labels=labels,
+            K=get_num_classes(pred_probs=pred_probs, labels=labels),
+        )
         label_issues_mask[mask] = False
         num_issues = np.sum(label_issues_mask)
     elif estimation_method == "off_diagonal_calibrated":
