434f32f0fb35d687ab1ea401280d1cce0c8da4e1
Elías Snorrason <eliassno@gmail.com>
2024-04-30 01:24:11 +0000

    Refactor test_all_identical_examples.py to use parametrize for dataset fixtures

--- tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
+++ tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
@@ -4,25 +4,23 @@ import pytest
 from cleanlab import Datalab
 
 
-SEED = 42
-
-
 class TestAllIdenticalExamplesDataset:
-    N = 20
-    K = 5
-
     @pytest.fixture
-    def dataset(self):
-        np.random.seed(SEED)
+    def dataset(self, request):
+        N, K = request.param
         # Create the dataset with all identical examples
-        X = np.full((self.N, self.K), fill_value=np.random.rand(self.K))
+        X = np.full((N, K), fill_value=np.random.rand(K))
         return {"X": X}
 
     @pytest.fixture
-    def dataset_with_one_unique_example(self, dataset):
+    def dataset_with_one_unique_example(self, request):
+        N, K = request.param
+        # Create the dataset with all identical examples
+        X = np.full((N, K), fill_value=np.random.rand(K))
+
         # Add one unique example to the dataset
-        dataset["X"] = np.vstack([dataset["X"], np.random.rand(self.K)])
-        return dataset
+        X = np.vstack([X, np.random.rand(K)])
+        return {"X": X}
 
     @pytest.fixture
     def lab(self, dataset):
@@ -32,13 +30,19 @@ class TestAllIdenticalExamplesDataset:
     def lab_with_one_unique_example(self, dataset_with_one_unique_example):
         return Datalab(data=dataset_with_one_unique_example)
 
-    def test_issue_detection(self, lab, dataset):
-
+    @pytest.mark.parametrize(
+        "dataset",
+        [((N, K)) for N in [11, 20, 50, 100, 150] for K in [2, 3, 5, 10, 20]],
+        indirect=["dataset"],
+        ids=lambda x: f"N={x[0]}, K={x[1]}",
+    )
+    def test_issue_detection(self, dataset):
+        lab = Datalab(data=dataset)
         lab.find_issues(features=dataset["X"])
 
         outlier_issues = lab.get_issues("outlier")
         expected_outlier_issues = pd.DataFrame(
-            [{"is_outlier_issue": False, "outlier_score": 1.0}] * self.N
+            [{"is_outlier_issue": False, "outlier_score": 1.0}] * len(outlier_issues)
         )
         pd.testing.assert_frame_equal(outlier_issues, expected_outlier_issues)
 
@@ -50,25 +54,30 @@ class TestAllIdenticalExamplesDataset:
                     "near_duplicate_score": 0.0,
                     "distance_to_nearest_neighbor": np.finfo(np.float64).epsneg,
                 }
-                for i in range(self.N)
+                for i in range(len(near_duplicate_issues))
             ]
         )
         pd.testing.assert_frame_equal(
             near_duplicate_issues.drop(columns="near_duplicate_sets"),
             expected_near_duplicate_issues,
+            check_exact=False,
+            atol=5e-16,
         )
 
-    def test_issue_detection_with_one_unique_example(
-        self, lab_with_one_unique_example, dataset_with_one_unique_example
-    ):
-        lab = lab_with_one_unique_example
+    @pytest.mark.parametrize(
+        "dataset_with_one_unique_example",
+        [((N, K)) for N in [11, 20, 50, 100, 150] for K in [2, 3, 5, 10, 20]],
+        indirect=["dataset_with_one_unique_example"],
+        ids=lambda x: f"N={x[0]}, K={x[1]}",
+    )
+    def test_issue_detection_with_one_unique_example(self, dataset_with_one_unique_example):
         dataset = dataset_with_one_unique_example
-
+        lab = Datalab(data=dataset)
         lab.find_issues(features=dataset["X"])
 
         outlier_issues = lab.get_issues("outlier")
         expected_outlier_issues = pd.DataFrame(
-            [{"is_outlier_issue": False, "outlier_score": 1.0}] * self.N
+            [{"is_outlier_issue": False, "outlier_score": 1.0}] * (len(outlier_issues) - 1)
             + [{"is_outlier_issue": True, "outlier_score": 0.0}]
         )
 
@@ -80,19 +89,19 @@ class TestAllIdenticalExamplesDataset:
                 {
                     "is_near_duplicate_issue": True,
                     "near_duplicate_score": 0.0,
-                    "distance_to_nearest_neighbor": np.finfo(np.float64).epsneg,
                 }
-                for i in range(self.N)
+                for i in range(len(near_duplicate_issues) - 1)
             ]
             + [
                 {
                     "is_near_duplicate_issue": False,
                     "near_duplicate_score": 1.0,
-                    "distance_to_nearest_neighbor": 3.212390e-01,
                 }
             ]
         )
         pd.testing.assert_frame_equal(
-            near_duplicate_issues.drop(columns="near_duplicate_sets"),
+            near_duplicate_issues.drop(
+                columns=["near_duplicate_sets", "distance_to_nearest_neighbor"]
+            ),
             expected_near_duplicate_issues,
         )
