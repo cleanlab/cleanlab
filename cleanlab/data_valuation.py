Fix numerical instability with Euclidean distance metric (#1113)

cleanlab/data_valuation.py
@@ -19,10 +19,11 @@ Data Valuation helps us assess individual training data points' contributions to
 """
 
 
-from typing import Optional, cast
+from typing import Callable, Optional, Union, cast
 
 import numpy as np
 from scipy.sparse import csr_matrix
+from scipy.spatial.distance import euclidean
 from sklearn.neighbors import NearestNeighbors
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
@@ -45,7 +46,7 @@ def _knn_shapley_score(knn_graph: csr_matrix, labels: np.ndarray, k: int) -> np.
 
 
 def _process_knn_graph_from_features(
-    features: np.ndarray, metric: Optional[str], k: int = 10
+    features: np.ndarray, metric: Optional[Union[str, Callable]], k: int = 10
 ) -> csr_matrix:
     """Calculate the knn graph from the features if it is not provided in the kwargs."""
     if k > len(features):  # Ensure number of neighbors less than number of examples
@@ -53,7 +54,11 @@ def _process_knn_graph_from_features(
             f"Number of nearest neighbors k={k} cannot exceed the number of examples N={len(features)} passed into the estimator (knn)."
         )
     if metric == None:
-        metric = "cosine" if features.shape[1] > 3 else "euclidean"
+        metric = (
+            "cosine"
+            if features.shape[1] > 3
+            else "euclidean" if features.shape[0] > 100 else euclidean
+        )
     knn = NearestNeighbors(n_neighbors=k, metric=metric).fit(features)
     knn_graph = knn.kneighbors_graph(mode="distance")
     try:
@@ -68,7 +73,7 @@ def data_shapley_knn(
     *,
     features: Optional[np.ndarray] = None,
     knn_graph: Optional[csr_matrix] = None,
-    metric: Optional[str] = None,
+    metric: Optional[Union[str, Callable]] = None,
     k: int = 10,
 ) -> np.ndarray:
     """
@@ -94,10 +99,12 @@ def data_shapley_knn(
             If provided, this must be a 2D array with shape (num_examples, num_features).
     knn_graph :
         A precomputed sparse KNN graph. If not provided, it will be computed from the `features` using the specified `metric`.
-    metric : Optional[str], default=None
+    metric : Optional[str or Callable], default=None
         The distance metric for KNN graph construction.
         Supports metrics available in ``sklearn.neighbors.NearestNeighbors``
         Default metric is ``"cosine"`` for ``dim(features) > 3``, otherwise ``"euclidean"`` for lower-dimensional data.
+        The euclidean is computed with an efficient implementation from scikit-learn when the number of examples is greater than 100.
+        When the number of examples is 100 or fewer, a more numerically stable version of the euclidean distance from scipy is used.
     k :
         The number of neighbors to consider for the KNN graph and Data Shapley value computation.
         Must be less than the total number of data points.

cleanlab/datalab/internal/issue_manager/duplicate.py
@@ -15,12 +15,13 @@
 # along with cleanlab.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, ClassVar, Dict, List, Optional, Union
 import warnings
 
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
+from scipy.spatial.distance import euclidean
 from sklearn.neighbors import NearestNeighbors
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
@@ -53,7 +54,7 @@ class NearDuplicateIssueManager(IssueManager):
     def __init__(
         self,
         datalab: Datalab,
-        metric: Optional[str] = None,
+        metric: Optional[Union[str, Callable]] = None,
         threshold: float = 0.13,
         k: int = 10,
         **_,
@@ -79,7 +80,11 @@ class NearDuplicateIssueManager(IssueManager):
                     "If a knn_graph is not provided, features must be provided to fit a new knn."
                 )
             if self.metric is None:
-                self.metric = "cosine" if features.shape[1] > 3 else "euclidean"
+                self.metric = (
+                    "cosine"
+                    if features.shape[1] > 3
+                    else "euclidean" if features.shape[0] > 100 else euclidean
+                )
             knn = NearestNeighbors(n_neighbors=self.k, metric=self.metric)
 
             if self.metric and self.metric != knn.metric:

cleanlab/outlier.py
@@ -21,9 +21,10 @@ The underlying algorithms are described in `this paper <https://arxiv.org/abs/22
 """
 
 import warnings
-from typing import Dict, Optional, Tuple, Union
+from typing import Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
+from scipy.spatial.distance import euclidean
 from sklearn.exceptions import NotFittedError
 from sklearn.neighbors import NearestNeighbors
 
@@ -60,9 +61,16 @@ class OutOfDistribution:
              You can also pass in a subclass of ``sklearn.neighbors.NearestNeighbors`` which allows you to use faster
              approximate neighbor libraries as long as you wrap them behind the same sklearn API.
              If you specify ``knn`` here, there is no need to later call ``fit()`` before calling ``score()``.
-             If ``knn = None``, then by default: ``knn = sklearn.neighbors.NearestNeighbors(n_neighbors=k, metric=dist_metric).fit(features)``
-             where ``dist_metric == "cosine"`` if ``dim(features) > 3`` or ``dist_metric == "euclidean"`` otherwise.
+             If ``knn is None``, then by default:
+             The knn object is instantiated as ``sklearn.neighbors.NearestNeighbors(n_neighbors=k, metric=dist_metric).fit(features)``.
+             - If ``dim(features) > 3``, the distance metric is set to "cosine".
+             - If ``dim(features) <= 3``, the distance metric is set to "euclidean".
+               The implementation of the euclidean distance metric depends on the number of examples in the features array:
+                - For more than 100 rows, it uses scikit-learn's "euclidean" metric. This is for efficiency reasons reasons.
+                - For 100 or fewer rows, it uses scipy's ``scipy.spatial.distance.euclidean`` metric. This is for numerical stability reasons.
              See: https://scikit-learn.org/stable/modules/neighbors.html
+             See: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.euclidean_distances.html
+             See: https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.euclidean.html
        *  k : int, default=None
              Optional number of neighbors to use when calculating outlier score (average distance to neighbors).
              If `k` is not provided, then by default ``k = knn.n_neighbors`` or ``k = 10`` if ``knn is None``.
@@ -413,6 +421,7 @@ class OutOfDistribution:
         """
         DEFAULT_K = 10
         # fit skip over (if knn is not None) then skipping fit and suggest score else fit.
+        distance_metric = None
         if knn is None:  # setup default KNN estimator
             # Make sure both knn and features are not None
             if features is None:
@@ -421,18 +430,24 @@ class OutOfDistribution:
                 )
             if k is None:
                 k = DEFAULT_K  # use default when knn and k are both None
-            if k > len(features):  # Ensure number of neighbors less than number of examples
+            N, M = features.shape
+            if k > N:  # Ensure number of neighbors less than number of examples
                 raise ValueError(
                     f"Number of nearest neighbors k={k} cannot exceed the number of examples N={len(features)} passed into the estimator (knn)."
                 )
 
-            if features.shape[1] > 3:  # use euclidean distance for lower dimensional spaces
+            # strings are used for sklearn metrics, callables are scipy pairwise distance functions
+            metric: Union[str, Callable]
+            if M > 3:  # use euclidean distance for lower dimensional spaces
                 metric = "cosine"
-            else:
+            elif N > 100:  # Use efficient implementation (numerically unstable in edge cases)
                 metric = "euclidean"
+            else:  # Use scipy implementation for precise results
+                metric = euclidean
 
             knn = NearestNeighbors(n_neighbors=k, metric=metric).fit(features)
             features = None  # features should be None in knn.kneighbors(features) to avoid counting duplicate data points
+            distance_metric = metric if isinstance(metric, str) else str(metric.__name__)
 
         elif k is None:
             k = knn.n_neighbors
@@ -472,12 +487,14 @@ class OutOfDistribution:
         ood_features_scores = transform_distances_to_scores(
             avg_knn_distances, t, scaling_factor=scaling_factor
         )
-        distance_metric = knn.metric
+        distance_metric = distance_metric or (
+            metric if isinstance((metric := knn.metric), str) else metric.__name__
+        )
         p = None
         if distance_metric == "minkowski":
             p = knn.p
         ood_features_scores = correct_precision_errors(
-            ood_features_scores, avg_knn_distances, knn.metric, p=p
+            ood_features_scores, avg_knn_distances, distance_metric, p=p
         )
         return (ood_features_scores, knn)
 

cleanlab/regression/rank.py
@@ -24,9 +24,10 @@ To obtain out-of-sample predictions for every datapoint in your dataset, you can
 If you have a sklearn-compatible regression model, consider using `cleanlab.regression.learn.CleanLearning` instead, which can more accurately identify noisy label values.
 """
 
-from typing import Dict, Callable
+from typing import Dict, Callable, Optional, Union
 import numpy as np
 from numpy.typing import ArrayLike
+from scipy.spatial.distance import euclidean
 from sklearn.neighbors import NearestNeighbors
 
 from cleanlab.outlier import OutOfDistribution
@@ -141,7 +142,7 @@ def _get_outre_score_for_each_label(
     *,
     residual_scale: float = 5,
     frac_neighbors: float = 0.5,
-    neighbor_metric: str = "euclidean",
+    neighbor_metric: Optional[Union[str, Callable]] = "euclidean",
 ) -> np.ndarray:
     """Returns OUTRE based label-quality scores.
 
@@ -162,8 +163,9 @@ def _get_outre_score_for_each_label(
     frac_neighbors: float, default = 0.5
         Fraction of examples in dataset that should be considered as `n_neighbors` in the ``NearestNeighbors`` object used internally to assess outliers.
 
-    neighbor_metric: str, default = "euclidean"
+    neighbor_metric: Optional[str or callable], default = None
         The parameter is passed to sklearn NearestNeighbors. # TODO add reference to sklearn.NearestNeighbor?
+        If None, the metric is chosen based on the number of features in the dataset.
 
     Returns
     -------
@@ -179,6 +181,11 @@ def _get_outre_score_for_each_label(
     features = np.array([labels, residual]).T
 
     neighbors = int(np.ceil(frac_neighbors * labels.shape[0]))
+    if neighbor_metric is None:
+        if features.shape[0] > 100:
+            neighbor_metric = "euclidean"
+        else:
+            neighbor_metric = euclidean
     knn = NearestNeighbors(n_neighbors=neighbors, metric=neighbor_metric).fit(features)
     ood = OutOfDistribution(params={"knn": knn})
 