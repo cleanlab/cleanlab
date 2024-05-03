182f50c73312159d0d4412f9d9892c7365019d17
Elías Snorrason <eliassno@gmail.com>
2024-05-03 20:53:43 +0000

    Add tests for detecting label issues in all identical examples dataset

--- tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
+++ tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
@@ -10,7 +10,13 @@ class TestAllIdenticalExamplesDataset:
         N, K = request.param
         # Create the dataset with all identical examples
         X = np.full((N, K), fill_value=np.random.rand(K))
-        return {"X": X}
+
+        # All labels for identical points should be the same
+        y = ["a"] * N
+        # One of the labels is different, so it should be flagged with a label issue
+        y[-1] = "b"
+
+        return {"X": X, "y": y}
 
     @pytest.fixture
     def dataset_with_one_unique_example(self, request):
@@ -20,7 +26,13 @@ class TestAllIdenticalExamplesDataset:
 
         # Add one unique example to the dataset
         X = np.vstack([X, np.random.rand(K)])
-        return {"X": X}
+
+        # All labels for identical points should be the same, let's make them all 0, along with the unique example
+        y = ["a"] * (N + 1)
+        # One of the N points has a noisy label, it should be flagged with a label issue
+        y[-2] = "b"
+
+        return {"X": X, "y": y}
 
     @pytest.fixture
     def lab(self, dataset):
@@ -37,8 +49,9 @@ class TestAllIdenticalExamplesDataset:
         ids=lambda x: f"N={x[0]}, K={x[1]}",
     )
     def test_issue_detection(self, dataset):
-        lab = Datalab(data=dataset)
-        lab.find_issues(features=dataset["X"])
+        lab = Datalab(data=dataset, label_name="y")
+        pred_probs = np.full((len(dataset["y"]), 2), fill_value=[1.0, 0.0])
+        lab.find_issues(features=dataset["X"], pred_probs=pred_probs)
 
         outlier_issues = lab.get_issues("outlier")
         expected_outlier_issues = pd.DataFrame(
@@ -64,6 +77,15 @@ class TestAllIdenticalExamplesDataset:
             atol=5e-16,
         )
 
+        lab_issues = lab.get_issues("label")[["is_label_issue", "label_score"]]
+        expected_lab_issues = pd.DataFrame(
+            [{"is_label_issue": False, "label_score": 1.0}] * (len(lab_issues) - 1)
+            + [
+                {"is_label_issue": False, "label_score": 0.0}
+            ]  # The confident threshold for this examble is extremely low, but won't flag it as a label issue
+        )
+        pd.testing.assert_frame_equal(lab_issues, expected_lab_issues)
+
     @pytest.mark.parametrize(
         "dataset_with_one_unique_example",
         [((N, K)) for N in [11, 20, 50, 100, 150] for K in [2, 3, 5, 10, 20]],
@@ -72,8 +94,9 @@ class TestAllIdenticalExamplesDataset:
     )
     def test_issue_detection_with_one_unique_example(self, dataset_with_one_unique_example):
         dataset = dataset_with_one_unique_example
-        lab = Datalab(data=dataset)
-        lab.find_issues(features=dataset["X"])
+        lab = Datalab(data=dataset, label_name="y")
+        pred_probs = np.full((len(dataset["y"]), 2), fill_value=[1.0, 0.0])
+        lab.find_issues(features=dataset["X"], pred_probs=pred_probs)
 
         outlier_issues = lab.get_issues("outlier")
         expected_outlier_issues = pd.DataFrame(
@@ -105,3 +128,13 @@ class TestAllIdenticalExamplesDataset:
             ),
             expected_near_duplicate_issues,
         )
+
+        lab_issues = lab.get_issues("label")[["is_label_issue", "label_score"]]
+        expected_lab_issues = pd.DataFrame(
+            [{"is_label_issue": False, "label_score": 1.0}] * (len(lab_issues) - 2)
+            + [
+                {"is_label_issue": False, "label_score": 0.0}
+            ]  # The confident threshold for this examble is extremely low, but won't flag it as a label issue
+            + [{"is_label_issue": False, "label_score": 1.0}]
+        )
+        pd.testing.assert_frame_equal(lab_issues, expected_lab_issues)
