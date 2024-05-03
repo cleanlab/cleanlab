a0c197c1355d35cbb040d476070ed4f195a36260
Elías Snorrason <eliassno@gmail.com>
2024-05-03 20:54:18 +0000

    Add tests for detecting label issues in all identical examples dataset

--- tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
+++ tests/datalab/datalab/end_to_end_tests/test_all_identical_examples.py
@@ -35,12 +35,50 @@ class TestAllIdenticalExamplesDataset:
         return {"X": X, "y": y}
 
     @pytest.fixture
+    def regression_dataset(self, request):
+        N, K = request.param
+        # Create the dataset with all identical examples
+        X = np.full((N, K), fill_value=np.random.rand(K))
+
+        # All labels for identical points should be the same
+        y = np.full(N, fill_value=np.random.rand())
+        # Flip one of the targets to introduce a label issue
+        y[-1] += 10
+
+        return {"X": X, "y": y}
+
+    @pytest.fixture
+    def regression_dataset_with_one_unique_example(self, request):
+        N, K = request.param
+        # Create the dataset with all identical examples
+        X = np.full((N, K), fill_value=np.random.rand(K))
+
+        # All labels for identical points should be the same
+        y = np.full(N, fill_value=np.random.rand())
+        # Flip one of the targets to introduce a label issue
+        y[-1] += 10
+
+        # Add one unique example to the dataset, but it has the same target as the majority of the dataset
+        X = np.vstack([X, np.random.rand(K)])
+        y = np.append(y, [y[0]])
+
+        return {"X": X, "y": y}
+
+    @pytest.fixture
     def lab(self, dataset):
         return Datalab(data=dataset)
 
     @pytest.fixture
     def lab_with_one_unique_example(self, dataset_with_one_unique_example):
         return Datalab(data=dataset_with_one_unique_example)
+
+    @pytest.fixture
+    def regression_lab(self, regression_dataset):
+        return Datalab(data=regression_dataset, task="regression")
+
+    @pytest.fixture
+    def regression_lab_with_one_unique_example(self, regression_dataset_with_one_unique_example):
+        return Datalab(data=regression_dataset_with_one_unique_example, task="regression")
 
     @pytest.mark.parametrize(
         "dataset",
@@ -138,3 +176,103 @@ class TestAllIdenticalExamplesDataset:
             + [{"is_label_issue": False, "label_score": 1.0}]
         )
         pd.testing.assert_frame_equal(lab_issues, expected_lab_issues)
+
+    @pytest.mark.parametrize(
+        "regression_dataset",
+        [((N, K)) for N in [11, 20, 50, 100] for K in [3, 4, 6, 8, 10]],
+        indirect=["regression_dataset"],
+        ids=lambda x: f"N={x[0]}, K={x[1]}",
+    )
+    def test_regression_issue_detection(self, regression_dataset):
+        lab = Datalab(data=regression_dataset, label_name="y", task="regression")
+        predictions = np.full(len(y := regression_dataset["y"]), fill_value=y[0])
+
+        lab.find_issues(pred_probs=predictions, features=regression_dataset["X"])
+
+        outlier_issues = lab.get_issues("outlier")
+        expected_outlier_issues = pd.DataFrame(
+            [{"is_outlier_issue": False, "outlier_score": 1.0}] * len(outlier_issues)
+        )
+        pd.testing.assert_frame_equal(outlier_issues, expected_outlier_issues)
+
+        near_duplicate_issues = lab.get_issues("near_duplicate")
+        expected_near_duplicate_issues = pd.DataFrame(
+            [
+                {
+                    "is_near_duplicate_issue": True,
+                    "near_duplicate_score": 0.0,
+                    "distance_to_nearest_neighbor": np.finfo(np.float64).epsneg,
+                }
+                for i in range(len(near_duplicate_issues))
+            ]
+        )
+        pd.testing.assert_frame_equal(
+            near_duplicate_issues.drop(columns="near_duplicate_sets"),
+            expected_near_duplicate_issues,
+            check_exact=False,
+            atol=5e-16,
+        )
+
+        label_issues = lab.get_issues("label")[["is_label_issue", "label_score"]]
+        expected_label_issues = pd.DataFrame(
+            [{"is_label_issue": False, "label_score": 1.0}] * (len(label_issues) - 1)
+            + [
+                {"is_label_issue": True, "label_score": 0.0}
+            ]  # Expect last example to be flagged as a label issue
+        )
+        pd.testing.assert_frame_equal(label_issues, expected_label_issues)
+
+    @pytest.mark.parametrize(
+        "regression_dataset_with_one_unique_example",
+        [((N, K)) for N in [11, 20, 50, 100] for K in [3, 4, 6, 8, 10]],
+        indirect=["regression_dataset_with_one_unique_example"],
+        ids=lambda x: f"N={x[0]}, K={x[1]}",
+    )
+    def test_regression_issue_detection_with_one_unique_example(
+        self, regression_dataset_with_one_unique_example
+    ):
+        dataset = regression_dataset_with_one_unique_example
+        lab = Datalab(data=dataset, label_name="y", task="regression")
+        predictions = np.full(len(y := dataset["y"]), fill_value=y[0])
+
+        lab.find_issues(pred_probs=predictions, features=dataset["X"])
+
+        outlier_issues = lab.get_issues("outlier")
+        expected_outlier_issues = pd.DataFrame(
+            [{"is_outlier_issue": False, "outlier_score": 1.0}] * (len(outlier_issues) - 1)
+            + [{"is_outlier_issue": True, "outlier_score": 0.0}]
+        )
+        pd.testing.assert_frame_equal(outlier_issues, expected_outlier_issues)
+
+        near_duplicate_issues = lab.get_issues("near_duplicate")
+        expected_near_duplicate_issues = pd.DataFrame(
+            [
+                {
+                    "is_near_duplicate_issue": True,
+                    "near_duplicate_score": 0.0,
+                }
+                for i in range(len(near_duplicate_issues) - 1)
+            ]
+            + [
+                {
+                    "is_near_duplicate_issue": False,
+                    "near_duplicate_score": 1.0,
+                }
+            ]
+        )
+        pd.testing.assert_frame_equal(
+            near_duplicate_issues.drop(
+                columns=["near_duplicate_sets", "distance_to_nearest_neighbor"]
+            ),
+            expected_near_duplicate_issues,
+        )
+
+        label_issues = lab.get_issues("label")[["is_label_issue", "label_score"]]
+        expected_label_issues = pd.DataFrame(
+            [{"is_label_issue": False, "label_score": 1.0}] * (len(label_issues) - 2)
+            + [
+                {"is_label_issue": True, "label_score": 0.0}
+            ]  # Expect second to last example to be flagged as a label issue
+            + [{"is_label_issue": False, "label_score": 1.0}]
+        )
+        pd.testing.assert_frame_equal(label_issues, expected_label_issues)
