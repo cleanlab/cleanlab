2c6571675f4e1ebf692a8cc6cd418ce230710a6b
Martin Zornoza Auñon <65114284+gogetron@users.noreply.github.com>
2024-05-23 11:24:21 +0100

    Remove extra white line util.py

--- cleanlab/internal/util.py
+++ cleanlab/internal/util.py
@@ -21,7 +21,6 @@ Ancillary helper methods used internally throughout this package; mostly related
 import warnings
 from typing import Optional, Tuple, Union
 
-
 import numpy as np
 import pandas as pd
 
