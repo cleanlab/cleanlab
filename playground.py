e7cef728b66ad743fbdd8c68418f494b2a7f10ca
Sam Davies Udina <sam.daviesudina@sensat.co.uk>
2024-10-24 11:41:16 +0100

    Wrap script in 'if name==main' to avoid recursive spawning

--- playground.py
+++ playground.py
@@ -9,5 +9,37 @@ from sentence_transformers import SentenceTransformer
 
 from cleanlab.classification import CleanLearning
 
-data = pd.read_csv("./banking-intent-classification.csv")
-print(data.head())
+
+def main():
+    data = pd.read_csv("./banking-intent-classification.csv")
+
+    raw_texts, raw_labels = data["text"].values, data["label"].values
+
+    raw_train_texts, raw_test_texts, raw_train_labels, raw_test_labels = train_test_split(
+        raw_texts, raw_labels, test_size=0.1
+    )
+    num_classes = len(set(raw_train_labels))
+
+    encoder = LabelEncoder()
+    encoder.fit(raw_train_labels)
+
+    train_labels = encoder.transform(raw_train_labels)
+    test_labels = encoder.transform(raw_test_labels)
+
+    transformer = SentenceTransformer("google/electra-small-discriminator")
+
+    train_texts = transformer.encode(raw_train_texts)
+    test_texts = transformer.encode(raw_test_texts)
+
+    model = LogisticRegression(max_iter=400)
+
+    cv_n_folds = 5
+    cl = CleanLearning(model, cv_n_folds=cv_n_folds)
+
+    label_issues = cl.find_label_issues(X=train_texts, labels=train_labels)
+
+    print(label_issues.head())
+
+
+if __name__ == "__main__":
+    main()
